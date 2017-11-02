9956143383152c5b57e4b808bd831cde0afa0f97
LeoComandini <leonardocomandini@hotmail.it>
2017-11-02 15:12:44 +0100

    add sighash and tests

--- scripts/DER_encoding.py
+++ scripts/DER_encoding.py
@@ -35,6 +35,13 @@ int_follows = b'\x02'
 double_0 = b'\x00'
 mid = b'\x80'
 
+sighash_all = b'\x01'
+sighash_none = b'\x02'
+sighash_single = b'\x03'
+sighash_all_anyonecanpay = b'\x81'
+sighash_none_anyonecanpay = b'\x82'
+sighash_single_anyonecanpay = b'\x83'
+
 def to_bytes_min_length(n, byteorder = "big"):
     assert type(n) == int and n >= 0, "n must be a positive int"
     return n.to_bytes(n.bit_length() // 8 + 1 , byteorder) # at least 1 byte, even if n = 0
@@ -43,19 +50,47 @@ def to_bytes_add00(n):
     n_bytes = to_bytes_min_length(n, "big")
     return (double_0 if n_bytes[:1] >= mid else b'') + n_bytes
 
-# include the sighash? for now the code doesn't do it    
-def get_DER_sig(r, s):
+# include the sighash here?
+def get_DER_sig(r, s, sighash = sighash_all):
     r_DER = to_bytes_add00(r)
     s_DER = to_bytes_add00(s)
     r_len = len(r_DER).to_bytes(1, "big")
     s_len = len(s_DER).to_bytes(1, "big")
     DER_sig = int_follows + r_len + r_DER + int_follows + s_len + s_DER
     total_len = len(DER_sig).to_bytes(1, "big")
-    return DER_follows + total_len + DER_sig
+    return DER_follows + total_len + DER_sig + sighash
 
 def test_DER():
-    r, s = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff, \
-           0x1111887998ffffffffffffffffffffffffffffffffffffffffffffffffffffff
-    print(get_DER_sig(r, s).hex())
+    DER73 = get_DER_sig(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        sighash_all)
+    DER72 = get_DER_sig(0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        sighash_all)
+    DER71 = get_DER_sig(0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        sighash_all)
+    DER71b = get_DER_sig(0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                         0x00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                         sighash_all)
+    DER70 = get_DER_sig(0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        0x007fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        sighash_all)
+    DER69 = get_DER_sig(0x007fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        0x007fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff,\
+                        sighash_all)
+
+    print("\nSig 73 bytes")
+    print(DER73.hex(), "  -- len:", len(DER73))
+    print("\nSig 72 bytes")
+    print(DER72.hex(), "  -- len:", len(DER72))
+    print("\nSig 71 bytes")
+    print(DER71.hex(), "  -- len:", len(DER71))
+    print("\nSig 71b bytes")
+    print(DER71b.hex(), "  -- len:", len(DER71b))
+    print("\nSig 70 bytes")
+    print(DER70.hex(), "  -- len:", len(DER70))
+    print("\nSig 69 bytes")
+    print(DER69.hex(), "  -- len:", len(DER69))
     
-# test_DER()
+test_DER()
