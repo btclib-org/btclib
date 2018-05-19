3c7623cfb638e6037a0988e98fc283592ab7f09e
Ferdinando M. Ametrano <ferdinando.ametrano.net>
2018-05-20 01:14:41 +0200

    proper PEP8 None comparison

--- scripts/EC-7_10_F263_G3_4_N280.py
+++ scripts/EC-7_10_F263_G3_4_N280.py
@@ -8,7 +8,7 @@ assert 4*a*a*a+27*b*b !=0, "zero discriminant"
 prime = 263;
 
 def checkPoint(P):
-  assert P == (None, None) or (P[0]*P[0]*P[0]+a*P[0]+b) % prime == (P[1]*P[1]) % prime
+  assert P[0] is None or (P[0]*P[0]*P[0]+a*P[0]+b) % prime == (P[1]*P[1]) % prime
   
 # A given generator specifies the group order
 G = (3, 4)
@@ -20,47 +20,55 @@ order = 280
 from FiniteFields import modInv
 
 def pointDouble(P):
-  if P[1] == 0:
+  if P[1] == 0 or P[0] is None:
     return (None, None)
   lam = ((3*P[0]*P[0]+a) * modInv(2*P[1], prime)) % prime
   x = (lam*lam-2*P[0]) % prime
   y = (lam*(P[0]-x)-P[1]) % prime
-  return (x,y)
+  return (x, y)
 
 def pointAdd(P, Q):
-  if Q[0] == P[0]:
-    return (None, None)
-  if Q[0] == None:
+  if Q[0] is None:
     return P
-  if P[0] == None:
+  if P[0] is None:
     return Q
-  if P == Q:
-    return pointDouble(P)
+  if Q[0] == P[0]:
+    if Q[1] == P[1]:
+      return pointDouble(P)
+    else:
+      return (None, None)
   lam = ((Q[1]-P[1]) * modInv(Q[0]-P[0], prime)) % prime
   x = (lam*lam-P[0]-Q[0]) % prime
   y = (lam*(P[0]-x)-P[1]) % prime
-  return (x,y)
+  return (x, y)
 
 # double & add
 def pointMultiply(n, P):
-  assert n!=0
+  if n == 0 or P[0] is None:
+    return (None, None)
   if n == 1:
     return P
-  elif n % 2 == 1: # addition when n is odd
+  n = n % order
+  if n % 2 == 1: # addition when n is odd
     return pointAdd(P, pointMultiply(n - 1, P))
-  else:            # doubling when n is even
+  else:          # doubling when n is even
     return pointMultiply(n//2, pointDouble(P))
 
 i = 1
-print(i, G)
-checkPoint(G)
+P = G
+checkPoint(P)
+print(i, P)
+assert P == pointMultiply(i, G)
 
 i = 2
 P = pointDouble(G)
 checkPoint(P)
 print(i, P)
+assert P == pointMultiply(i, G)
 
 for i in range(3, order+2):
   P = pointAdd(P, G)
   checkPoint(P)
   print(i, P)
+  assert P == pointMultiply(i, G), pointMultiply(i, G)
+  
--- scripts/ECsecp256k1.py
+++ scripts/ECsecp256k1.py
@@ -11,7 +11,7 @@ prime = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 -1
 prime = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
 
 def checkPoint(P):
-  assert P == (None, None) or (P[0]*P[0]*P[0]+a*P[0]+b) % prime == (P[1]*P[1]) % prime
+  assert P[0] is None or (P[0]*P[0]*P[0]+a*P[0]+b) % prime == (P[1]*P[1]) % prime
   
 # A given generator specifies the group order
 gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
@@ -25,33 +25,36 @@ order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
 from FiniteFields import modInv
 
 def pointDouble(P):
-  if P[1] == 0:
+  if P[1] == 0 or P[0] is None:
     return (None, None)
   lam = ((3*P[0]*P[0]+a) * modInv(2*P[1], prime)) % prime
   x = (lam*lam-2*P[0]) % prime
   y = (lam*(P[0]-x)-P[1]) % prime
-  return (x,y)
+  return (x, y)
 
 def pointAdd(P, Q):
-  if Q[0] == P[0]:
-    return (None, None)
-  if Q[0] == None:
+  if Q[0] is None:
     return P
-  if P[0] == None:
+  if P[0] is None:
     return Q
-  if P == Q:
-    return pointDouble(P)
+  if Q[0] == P[0]:
+    if Q[1] == P[1]:
+      return pointDouble(P)
+    else:
+      return (None, None)
   lam = ((Q[1]-P[1]) * modInv(Q[0]-P[0], prime)) % prime
   x = (lam*lam-P[0]-Q[0]) % prime
   y = (lam*(P[0]-x)-P[1]) % prime
-  return (x,y)
+  return (x, y)
 
 # double & add
 def pointMultiply(n, P):
-  assert n!=0
+  if n == 0 or P[0] is None:
+    return (None, None)
   if n == 1:
     return P
-  elif n % 2 == 1: # addition when n is odd
+  n = n % order
+  if n % 2 == 1: # addition when n is odd
     return pointAdd(P, pointMultiply(n - 1, P))
-  else:            # doubling when n is even
+  else:          # doubling when n is even
     return pointMultiply(n//2, pointDouble(P))
