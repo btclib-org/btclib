55035cc2bdc873ae1cbcff8026106e97c1601a84
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2018-06-03 16:42:10 +0200

    added space

--- scripts/ECsecp256k1.py
+++ scripts/ECsecp256k1.py
@@ -2,7 +2,7 @@
 
 # secp256k1
 __a = 0; __b = 7
-__prime = 2**256 - 2**32 -977
+__prime = 2**256 - 2**32 - 977
 __gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
 __gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
 __G = (__gx, __gy)
