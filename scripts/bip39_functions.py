bba6a5a146f5dd493de59f8c91404a171d1c6d37
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2018-06-04 22:54:18 +0200

    generalized seed from mnemonic for electrum

--- scripts/bip39_functions.py
+++ scripts/bip39_functions.py
@@ -48,14 +48,17 @@ def bip39_mnemonic_from_ints(ints, dict_txt = 'dict_eng.txt'):
     mnemonic += ' ' + dictionary[j][:-1]
   return mnemonic[1:]
 
-def bip39_seed_from_mnemonic(mnemonic, passphrase='TREZOR'):
+def bip39_seed_from_mnemonic(mnemonic, passphrase, style='bip39'):
   # Function that derives the seed from the mnemonic phrase + the passphrase
   # INPUT:
   #   mnemonic: valid BIP39 mnemonic phrase
   #   passphrase: passphrase used for the seed derivation ('TREZOR' as default for the BIP39 test vector)
   # OUTPUT:
-  #   seed: seed for the BIP32 HD wallet  
-  return PBKDF2(mnemonic, 'mnemonic' + passphrase, 2048, sha512).read(64) # 512-bit
+  #   seed: seed for the BIP32 HD wallet
+  prefix = 'mnemonic'
+  if style == 'electrum': prefix = 'electrum'
+  else: assert style=='bip39'
+  return PBKDF2(mnemonic, prefix + passphrase, 2048, sha512).read(64) # 512 bits
 
 
 # Test vectors: 
@@ -69,7 +72,7 @@ def test_vector_1():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04"
 
 def test_vector_2():
@@ -77,7 +80,7 @@ def test_vector_2():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "2e8905819b8723fe2c1d161860e5ee1830318dbf49a83bd451cfb8440c28bd6fa457fe1296106559a3c80937a1c1069be3a3a5bd381ee6260e8d9739fce1f607"
 
 def test_vector_3():
@@ -85,7 +88,7 @@ def test_vector_3():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "d71de856f81a8acc65e6fc851a38d4d7ec216fd0796d0a6827a3ad6ed5511a30fa280f12eb2e47ed2ac03b5c462a0358d18d69fe4f985ec81778c1b370b652a8"
 
 def test_vector_4():
@@ -93,7 +96,7 @@ def test_vector_4():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "ac27495480225222079d7be181583751e86f571027b0497b5b5d11218e0a8a13332572917f0f8e5a589620c6f15b11c61dee327651a14c34e18231052e48c069"
 
 def test_vector_5():
@@ -101,7 +104,7 @@ def test_vector_5():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "035895f2f481b1b0f01fcf8c289c794660b289981a78f8106447707fdd9666ca06da5a9a565181599b79f53b844d8a71dd9f439c52a3d7b3e8a79c906ac845fa"
 
 def test_vector_6():
@@ -109,7 +112,7 @@ def test_vector_6():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "f2b94508732bcbacbcc020faefecfc89feafa6649a5491b8c952cede496c214a0c7b3c392d168748f2d4a612bada0753b52a1c7ac53c1e93abd5c6320b9e95dd"
 
 def test_vector_7():
@@ -117,7 +120,7 @@ def test_vector_7():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "107d7c02a5aa6f38c58083ff74f04c607c2d2c0ecc55501dadd72d025b751bc27fe913ffb796f841c49b1d33b610cf0e91d3aa239027f5e99fe4ce9e5088cd65"
 
 def test_vector_8():
@@ -125,7 +128,7 @@ def test_vector_8():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "0cd6e5d827bb62eb8fc1e262254223817fd068a74b5b449cc2f667c3f1f985a76379b43348d952e2265b4cd129090758b3e3c2c49103b5051aac2eaeb890a528"
 
 def test_vector_9():
@@ -133,7 +136,7 @@ def test_vector_9():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "bda85446c68413707090a52022edd26a1c9462295029f2e60cd7c4f2bbd3097170af7a4d73245cafa9c3cca8d561a7c3de6f5d4a10be8ed2a5e608d68f92fcc8"
 
 def test_vector_10():
@@ -141,7 +144,7 @@ def test_vector_10():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "bc09fca1804f7e69da93c2f2028eb238c227f2e9dda30cd63699232578480a4021b146ad717fbb7e451ce9eb835f43620bf5c514db0f8add49f5d121449d3e87"
 
 def test_vector_11():
@@ -149,7 +152,7 @@ def test_vector_11():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "c0c519bd0e91a2ed54357d9d1ebef6f5af218a153624cf4f2da911a0ed8f7a09e2ef61af0aca007096df430022f7a2b6fb91661a9589097069720d015e4e982f"
 
 def test_vector_12():
@@ -157,7 +160,7 @@ def test_vector_12():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "dd48c104698c30cfe2b6142103248622fb7bb0ff692eebb00089b32d22484e1613912f0a5b694407be899ffd31ed3992c456cdf60f5d4564b8ba3f05a69890ad"
 
 def test_vector_13():
@@ -165,7 +168,7 @@ def test_vector_13():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "274ddc525802f7c828d8ef7ddbcdc5304e87ac3535913611fbbfa986d0c9e5476c91689f9c8a54fd55bd38606aa6a8595ad213d4c9c9f9aca3fb217069a41028"
 
 def test_vector_14():
@@ -173,7 +176,7 @@ def test_vector_14():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "628c3827a8823298ee685db84f55caa34b5cc195a778e52d45f59bcf75aba68e4d7590e101dc414bc1bbd5737666fbbef35d1f1903953b66624f910feef245ac"
 
 def test_vector_15():
@@ -181,7 +184,7 @@ def test_vector_15():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "64c87cde7e12ecf6704ab95bb1408bef047c22db4cc7491c4271d170a1b213d20b385bc1588d9c7b38f1b39d415665b8a9030c9ec653d75e65f847d8fc1fc440"
 
 def test_vector_16():
@@ -189,7 +192,7 @@ def test_vector_16():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "ea725895aaae8d4c1cf682c1bfd2d358d52ed9f0f0591131b559e2724bb234fca05aa9c02c57407e04ee9dc3b454aa63fbff483a8b11de949624b9f1831a9612"
 
 def test_vector_17():
@@ -197,7 +200,7 @@ def test_vector_17():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "fd579828af3da1d32544ce4db5c73d53fc8acc4ddb1e3b251a31179cdb71e853c56d2fcb11aed39898ce6c34b10b5382772db8796e52837b54468aeb312cfc3d"
 
 def test_vector_18():
@@ -205,7 +208,7 @@ def test_vector_18():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "72be8e052fc4919d2adf28d5306b5474b0069df35b02303de8c1729c9538dbb6fc2d731d5f832193cd9fb6aeecbc469594a70e3dd50811b5067f3b88b28c3e8d"
 
 def test_vector_19():
@@ -213,7 +216,7 @@ def test_vector_19():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "deb5f45449e615feff5640f2e49f933ff51895de3b4381832b3139941c57b59205a42480c52175b6efcffaa58a2503887c1e8b363a707256bdd2b587b46541f5"
 
 def test_vector_20():
@@ -221,7 +224,7 @@ def test_vector_20():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "4cbdff1ca2db800fd61cae72a57475fdc6bab03e441fd63f96dabd1f183ef5b782925f00105f318309a7e9c3ea6967c7801e46c8a58082674c860a37b93eda02"
 
 def test_vector_21():
@@ -229,7 +232,7 @@ def test_vector_21():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "26e975ec644423f4a4c4f4215ef09b4bd7ef924e85d1d17c4cf3f136c2863cf6df0a475045652c57eb5fb41513ca2a2d67722b77e954b4b3fc11f7590449191d"
 
 def test_vector_22():
@@ -237,7 +240,7 @@ def test_vector_22():
   ENT = 128
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "2aaa9242daafcee6aa9d7269f17d4efe271e1b9a529178d7dc139cd18747090bf9d60295d0ce74309a78852a9caadf0af48aae1c6253839624076224374bc63f"
 
 def test_vector_23():
@@ -245,7 +248,7 @@ def test_vector_23():
   ENT = 192
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "7b4a10be9d98e6cba265566db7f136718e1398c71cb581e1b2f464cac1ceedf4f3e274dc270003c670ad8d02c4558b2f8e39edea2775c9e232c7cb798b069e88"
 
 def test_vector_24():
@@ -253,7 +256,7 @@ def test_vector_24():
   ENT = 256
   ints = bip39_ints_from_entropy(entropy, ENT)
   mnemonic = bip39_mnemonic_from_ints(ints)
-  seed = bip39_seed_from_mnemonic(mnemonic)
+  seed = bip39_seed_from_mnemonic(mnemonic, "TREZOR")
   assert seed.hex() == "01f5bced59dec48e362f2c45b5de68b9fd6c92c6634f44d6d40aab69056506f0e35524a518034ddc1192e1dacd32c1ed3eaa3c3b131c88ed8e7e54c49a5d0998"
 
 def test_vector():
