a0e6d1bf2db2767563a64882fee81fe25ebd2808
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2018-06-07 18:13:35 +0200

    updated

--- scripts/conversions.py
+++ scripts/conversions.py
@@ -1,39 +1,32 @@
 
-# Endianness refers to the sequential order in which bytes are arranged
-# - big endian: ordered from the big end, i.e. most significant bit first
-# - little endian: ordered from the little end, i.e. least significant bit first
+print("\nfrom hexstring to bytes, then back:")
+xhexstr = "000f"
+print("hexstring", xhexstr)
+#import codecs; xbytes = codecs.decode(xhexstr, 'hex')
+#xbytes = int(xhexstr, 16).to_bytes(2, 'big') # also handle "00f" and "0x000f", but you must know how many bytes
+xbytes = bytes.fromhex(xhexstr)
+print("bytes", xbytes)
+assert xhexstr == xbytes.hex()
+xhexstr = xbytes.hex()
+print("hexstring", xhexstr)
 
-xbytes = b'\x00\x0f' # assume big endian
-print("x is " + ("" if isinstance(xbytes, bytes) else "not ") + "bytes; as bytes:", xbytes)
-print("x is " + ("" if isinstance(xbytes, int) else "not ") + "int; as int:", int.from_bytes(xbytes, 'big'))
-print("x is " + ("" if isinstance(xbytes, str) else "not ") + "str; as hex string:", xbytes.hex())
-print()
-
+print("\nfrom int to hex string, then back:")
 xint = 15
-print("x is " + ("" if isinstance(xint, bytes) else "not ") + "bytes; as bytes:", xint.to_bytes(2, 'big'))
-print("x is " + ("" if isinstance(xint, int) else "not ") + "int; as int:", xint)
-# fixed length (4) with trailing 0s, and lower capitalization for letters (x); '#06x' for 4 digits with 0x prefix
-print("x is " + ("" if isinstance(xint, str) else "not ") + "str; as 4 character hex string:", format(xint, '04x'))
-# shortest possible string with 0x prefix
-#print("x is " + ("" if isinstance(xint, str) else "not ") + "str; as hex string:", hex(xint))
-print()
+print("int", xint)
+xhexstr = format(xint, '04x')
+print("hexstring", xhexstr, "(how many hexdigits, i.e. bytes, must be known in advance)")
+assert xint == int(xhexstr, 16)
+xint = int(xhexstr, 16)
+print("int", xint)
 
-# it would be better to avoid strings as input... anyway
+print("\nfrom int to bytes, then back:")
+xint = 15
+print("int", xint)
+xbytes = xint.to_bytes(2, 'big')
+print("bytes", xbytes, "(how many bytes must be known in advance)")
+assert xint == int.from_bytes(xbytes, 'big')
+xint = int.from_bytes(xbytes, 'big')
+print("int", xint)
 
-xstr = "000f" # must be even lenght for conversion to bytes to work
-print("x is " + ("" if isinstance(xstr, bytes) else "not ") + "bytes; as bytes:", bytes.fromhex(xstr))
-print("x is " + ("" if isinstance(xstr, int) else "not ") + "int; as int:", int(xstr, 16))
-print("x is " + ("" if isinstance(xstr, str) else "not ") + "str; as hex string:", xstr)
-print()
-
-xstr = "00f" # alternatively, for bytes conversion, it is more robust to pivot on int, then bytes
-print("x is " + ("" if isinstance(xstr, bytes) else "not ") + "bytes; as bytes:", int(xstr, 16).to_bytes(2, 'big'))
-print("x is " + ("" if isinstance(xstr, int) else "not ") + "int; as int:", int(xstr, 16))
-print("x is " + ("" if isinstance(xstr, str) else "not ") + "str; as hex string:", xstr)
-print()
-
-xstr = "0x000f" # alternatively, for bytes conversion, it is more rubust to pivot on int, then bytes
-print("x is " + ("" if isinstance(xstr, bytes) else "not ") + "bytes; as bytes:", int(xstr, 16).to_bytes(2, 'big'))
-print("x is " + ("" if isinstance(xstr, int) else "not ") + "int; as int:", int(xstr, 16))
-print("x is " + ("" if isinstance(xstr, str) else "not ") + "str; as hex string:", xstr)
-print()
+t = 0
+print(type(t == str))
