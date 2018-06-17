a7d7d81440ddcf3c4307fbe9018dca9cac9133cd
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2018-06-17 02:47:04 +0200

    tests in their own file

--- scripts/rfc6979.py
+++ scripts/rfc6979.py
@@ -73,19 +73,3 @@ def rfc6979_raw(prv, m, hasher=default_hasher):
             return nonce
         k = hmac_new(k, v + b'\x00', hasher).digest()
         v = hmac_new(k, v, hasher).digest()
-
-import unittest
-
-class Testrfc6979(unittest.TestCase):
-    def test_rfc6979(self):
-        # source: https://bitcointalk.org/index.php?topic=285142.40
-        msg = sha256(b'Satoshi Nakamoto').digest()
-        x = 0x1
-        nonce = rfc6979(x, msg)
-        expected = 0x8F8A276C19F4149656B280621E358CCE24F5F52542772691EE69063B74F15D15
-        self.assertEqual(nonce, expected)
-
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    unittest.main()
