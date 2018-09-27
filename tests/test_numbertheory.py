d089b9311ecdc39dc5d789f219914e5a56dacb5d
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2018-09-27 12:04:22 +0200

    improved test

--- tests/test_numbertheory.py
+++ tests/test_numbertheory.py
@@ -6,15 +6,18 @@ from btclib.numbertheory import mod_inv, mod_sqrt
 class TestNumberTheory(unittest.TestCase):
     def test_mod_sqrt(self):
         for p in [3, 5, 7, 11, 13, 17, 19, 23, 29]:
+            hasRoot = set()
+            hasRoot.add(1)
+            for i in range(2, p):
+                hasRoot.add(i*i % p)
             for i in range(1, p):
-                try:
+                if i in hasRoot:
                     root = mod_sqrt(i, p)
-                    #self.assertEqual(i, (root*root) % p)
+                    self.assertEqual(i, (root*root) % p)
                     root = p - root
-                    #self.assertEqual(i, (root*root) % p)
-                except ValueError:
-                    inv = mod_inv(i, p)
-                    self.assertEqual((i* inv) % p, 1)
+                    self.assertEqual(i, (root*root) % p)
+                else:
+                    self.assertRaises(ValueError, mod_sqrt, i, p)
 
     def test_mod_inv(self):
         for p in [3, 5, 7, 11, 13, 17, 19, 23, 29]:
