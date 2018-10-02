8bc6d2ca742f9f1103fb84253d6b5f2962882452
Giona Soldati <gionasoldati@gmail.com>
2018-10-02 15:07:16 +0200

    Added a test to check whether p - 1 is a quadratic residue or not

--- tests/test_numbertheory.py
+++ tests/test_numbertheory.py
@@ -24,7 +24,24 @@ class TestNumberTheory(unittest.TestCase):
             for i in range(1, p):
                 inv = mod_inv(i, p)
                 self.assertEqual((i* inv) % p, 1)
-                
+
+# Test that checks whether p - 1 (i.e. - 1 (mod p))
+    # is a quadratic residue (should be so in case p = 1 (mod 4)) or not
+    # (this should be the case if p = 3 (mod 4))
+    def test_minus_one_quadr_res(self):
+        for p in [3, 5, 7, 11, 13, 17, 19, 23, 29]:
+            hasRoot = set()
+            hasRoot.add(1)
+            for i in range(2, p):
+                hasRoot.add(i * i % p)
+            if (p % 4) == 3:
+                self.assertNotIn(p - 1, hasRoot)
+                self.assertRaises(ValueError, mod_sqrt, p - 1, p)
+            elif (p % 4) == 1:
+                self.assertIn(p - 1, hasRoot)
+                root = mod_sqrt(p - 1, p)
+                self.assertEqual(p - 1, root * root % p)
+
 if __name__ == "__main__":
     # execute only if run as a script
     unittest.main()
