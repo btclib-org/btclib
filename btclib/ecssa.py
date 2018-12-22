7fe1aff139385b95a0314125fe75744e1e31e7bc
gionasoldati <39330296+gionasoldati@users.noreply.github.com>
2018-12-22 13:05:36 +0100

    Batch validation (#22)
    Added batch validation following Schnorr's BIP: the (log) speed up is obtained via Bos-Coster's algorithm.

--- btclib/ecssa.py
+++ btclib/ecssa.py
@@ -8,10 +8,17 @@ from btclib.ellipticcurves import Union, Tuple, Optional, \
                                   Scalar as PrvKey, \
                                   Point as PubKey, GenericPoint as GenericPubKey, \
                                   mod_inv, \
-                                  EllipticCurve, \
-                                  int_from_Scalar, tuple_from_Point, bytes_from_Point
+                                  EllipticCurve, secp256k1 as ec, \
+                                  int_from_Scalar, tuple_from_Point, bytes_from_Point, \
+                                  pointAdd, pointAddJacobian, pointMultiplyJacobian
 from btclib.rfc6979 import rfc6979
 from btclib.ecsignutils import Message, Signature, int_from_hash
+import heapq
+
+
+PubKeys = Tuple[PubKey, ...]
+Messages = Tuple[Message, ...]
+Signatures = Tuple[Signature, ...]
 
 # %% ecssa sign
 # https://github.com/sipa/bips/blob/bip-schnorr/bip-schnorr.mediawiki
@@ -28,7 +35,7 @@ def ecssa_sign(ec: EllipticCurve, m: Message, q: PrvKey, eph_prv: Optional[PrvKe
 
 # https://eprint.iacr.org/2018/068
 def ecssa_sign_raw(ec: EllipticCurve, m: bytes, q: int, eph_prv: int, hasher = sha256) -> Signature:
-    K = ec.pointMultiply(eph_prv, ec.G)
+    K = pointMultiplyJacobian(ec, eph_prv, ec.G)
     assert K != None, 'sign fail'
     # break the simmetry: any criteria could be used, jacobi is standard
     if ec.jacobi(K[1]) != 1:
@@ -36,7 +43,7 @@ def ecssa_sign_raw(ec: EllipticCurve, m: bytes, q: int, eph_prv: int, hasher = s
         # let's fix eph_prv instead, as it is used later
         eph_prv = ec.n - eph_prv
     e = hasher(K[0].to_bytes(ec.bytesize, byteorder="big") +
-               bytes_from_Point(ec, ec.pointMultiply(q, ec.G), True) +
+               bytes_from_Point(ec, pointMultiplyJacobian(ec, q, ec.G), True) +
                m).digest()
     e = int_from_hash(e, ec.n) % ec.n
     assert e != 0, "sign fail"
@@ -59,7 +66,7 @@ def ecssa_verify_raw(ec: EllipticCurve, m: bytes, ssasig: Signature, Q: PubKey, 
     e = hasher(r.to_bytes(ec.bytesize, byteorder="big") + bytes_from_Point(ec, Q, True) + m).digest()
     e = int_from_hash(e, ec.n) % ec.n
     # R = sG - eP
-    K = ec.pointAdd(ec.pointMultiply(s, ec.G), ec.pointMultiply(ec.n - e, Q))
+    K = pointAdd(ec, pointMultiplyJacobian(ec, s, ec.G), pointMultiplyJacobian(ec, ec.n - e, Q))
     if K is None or ec.jacobi(K[1]) != 1:
         return False
     return K[0] == ssasig[0]
@@ -77,8 +84,8 @@ def ecssa_pubkey_recovery_raw(ec: EllipticCurve, e: bytes, ssasig: Signature) ->
     e = int_from_hash(e, ec.n) % ec.n
     assert e != 0, "invalid challenge e"
     e1 = mod_inv(e, ec.n)
-    return ec.pointAdd(ec.pointMultiply((e1 * s) % ec.n, ec.G),
-                       ec.pointMultiply(ec.n - e1, K))
+    return pointAdd(ec, pointMultiplyJacobian(ec, (e1 * s) % ec.n, ec.G),
+                       pointMultiplyJacobian(ec, ec.n - e1, K))
 
 
 def check_ssasig(ec: EllipticCurve, ssasig: Signature) -> bool:
@@ -91,3 +98,54 @@ def check_ssasig(ec: EllipticCurve, ssasig: Signature) -> bool:
     # FIXME: it might be 0 <= ssasig[1]
     assert 0 < ssasig[1] and ssasig[1] < ec.n, "s must be in [1..n]"
     return True
+
+def ecssa_batch_validation(u: int, Q: PubKeys, m: Messages, sigma: Signatures, a: Tuple[int, ...], hasher = sha256) -> bool:
+    if u == 1:
+        return ecssa_verify(ec, m[0], sigma[0], Q[0], hasher)
+    assert len(Q) == u and len(m) == u and len(sigma) == u and len(a) == u
+    for i in range(0, u):
+        if type(m[i]) == str: m[i] = hasher(m[i].encode()).digest()
+        check_ssasig(ec, sigma[i])
+        Q[i] =  tuple_from_Point(ec, Q[i])
+    return ecssa_batch_validation_raw(u, Q, m, sigma, a, hasher)
+
+def ecssa_batch_validation_raw(u: int, Q: PubKeys, m: Messages, sigma: Signatures, a: Tuple[int, ...], hasher = sha256) -> bool:
+    # initialization
+    mult = 0
+    points = list()
+    factors = list()
+
+    for i in range(0, u):
+        r, s = sigma[i]
+        e = hasher(r.to_bytes(32, byteorder="big") + bytes_from_Point(ec, Q[i], True) + m[i]).digest()
+        e = int_from_hash(e, ec.n)
+
+        c = (pow(r, 3) + 7) % ec._EllipticCurve__p
+        y = pow(c, (ec._EllipticCurve__p + 1) // 4, ec._EllipticCurve__p)
+        assert pow(y, 2, ec._EllipticCurve__p) == c
+
+        mult += a[i] * s % ec.n
+        points.append(ec.jac_from_affine((r, y)))
+        factors.append(a[i])
+        points.append(ec.jac_from_affine(Q[i]))
+        factors.append(a[i] * e % ec.n)
+
+    # Bos-coster's algorithm, source:
+    # https://cr.yp.to/badbatch/boscoster2.py
+    boscoster = list(zip([-n for n in factors], points))
+    heapq.heapify(boscoster)
+    while len(boscoster) > 1:
+        aK1 = heapq.heappop(boscoster)
+        aK2 = heapq.heappop(boscoster)
+        a1, K1 = -aK1[0], aK1[1]
+        a2, K2 = -aK2[0], aK2[1]
+        K2 = pointAddJacobian(ec, K1, K2)
+        a1 -= a2
+        if a1 > 0: 
+            heapq.heappush(boscoster,(-a1, K1))
+        heapq.heappush(boscoster,(-a2, K2))
+        
+    aK = heapq.heappop(boscoster)
+    RHS = pointMultiplyJacobian(ec, -aK[0], aK[1])
+    
+    return pointMultiplyJacobian(ec, mult, ec.G) == RHS
