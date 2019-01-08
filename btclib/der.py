bc608a543f33bcf6c121bbc4f140666e96ad9c49
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-01-09 00:20:06 +0100

    restored copyright

--- btclib/der.py
+++ btclib/der.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2017-2019 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
 """ 
     ===================
     Strict DER encoding
