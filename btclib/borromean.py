b439670c666911d383b7ee6d36ab2226d72b10dc
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-01-29 18:55:20 +0100

    made some functions private

--- btclib/borromean.py
+++ btclib/borromean.py
@@ -17,17 +17,18 @@ from btclib.ec import Point, pointMult, DblScalarMult
 from btclib.curves import secp256k1    
 from btclib.utils import bits2int, point2octets, octets2point
 
-# FIXME should bu urandom, but then tests would be non-deterministic
+# FIXME: should be urandom, but then tests would be non-deterministic
 random.seed(42)
 
-ec = secp256k1
+ec = secp256k1 # FIXME: any curve
+hf = sha256 # FIXME: any hf
 
-def borromean_hash(msg: bytes, R: bytes, i: int, j: int) -> bytes:
+def _borromean_hash(msg: bytes, R: bytes, i: int, j: int) -> bytes:
     temp = msg + R + i.to_bytes(4, 'big') + j.to_bytes(4, 'big')
-    return sha256(temp).digest()
+    return hf(temp).digest()
 
 
-def get_msg_format(msg: bytes, pubk_rings: Dict[int, List[Point]]) -> bytes:
+def _get_msg_format(msg: bytes, pubk_rings: Dict[int, List[Point]]) -> bytes:
     m = msg
     rings = len(pubk_rings)
     for i in range(rings):
@@ -36,7 +37,7 @@ def get_msg_format(msg: bytes, pubk_rings: Dict[int, List[Point]]) -> bytes:
             P = pubk_rings[i][j]
             Pbytes = point2octets(ec, P, True)
             m += Pbytes
-    return sha256(m).digest()
+    return hf(m).digest()
 
 
 def borromean_sign(msg: bytes,
@@ -58,7 +59,7 @@ def borromean_sign(msg: bytes,
 
     s: Dict[int, List[int]] = defaultdict(list)
     e: Dict[int, List[int]] = defaultdict(list)
-    m = get_msg_format(msg, pubk_rings)
+    m = _get_msg_format(msg, pubk_rings)
     e0bytes = m
     ring_size = len(pubk_rings)
     # step 1
@@ -72,22 +73,22 @@ def borromean_sign(msg: bytes,
         if start_idx != 0:
             for j in range(start_idx, keys_size):
                 s[i][j] = random.getrandbits(256)
-                e[i][j] = bits2int(ec, borromean_hash(m, R, i, j))
+                e[i][j] = bits2int(ec, _borromean_hash(m, R, i, j))
                 assert 0 < e[i][j] < ec.n, "sign fail: how did you do that?!?"
                 T = DblScalarMult(ec, s[i][j], ec.G, -e[i][j], pubk_rings[i][j])
                 R = point2octets(ec, T, True)
         e0bytes += R
-    e0 = sha256(e0bytes).digest()
+    e0 = hf(e0bytes).digest()
     # step 2
     for i in range(ring_size):
-        e[i][0] = bits2int(ec, borromean_hash(m, e0, i, 0))
+        e[i][0] = bits2int(ec, _borromean_hash(m, e0, i, 0))
         assert 0 < e[i][0] < ec.n, "sign fail: how did you do that?!?"
         j_star = sign_key_idx[i]
         for j in range(1, j_star+1):
             s[i][j-1] = random.getrandbits(256)
             T = DblScalarMult(ec, s[i][j-1], ec.G, -e[i][j-1], pubk_rings[i][j-1])
             R = point2octets(ec, T, True)
-            e[i][j] = bits2int(ec, borromean_hash(m, R, i, j))
+            e[i][j] = bits2int(ec, _borromean_hash(m, R, i, j))
             assert 0 < e[i][j] < ec.n, "sign fail: how did you do that?!?"
         s[i][j_star] = k[i] + sign_keys[i]*e[i][j_star]
     return e0, s
@@ -120,22 +121,22 @@ def _borromean_verify(msg: bytes,
                       pubk_rings: Dict[int, List[Point]]) -> bool:
 
     ring_size = len(pubk_rings)
-    m = get_msg_format(msg, pubk_rings)
+    m = _get_msg_format(msg, pubk_rings)
     e: Dict[int, List[int]] = defaultdict(list)
     e0bytes = m
     for i in range(ring_size):
         keys_size = len(pubk_rings[i])
         e[i] = [0]*keys_size
-        e[i][0] = bits2int(ec, borromean_hash(m, e0, i, 0))
+        e[i][0] = bits2int(ec, _borromean_hash(m, e0, i, 0))
         assert e[i][0] != 0, "invalid sig: how did you do that?!?"
         R = b'\0x00'
         for j in range(keys_size):
             T = DblScalarMult(ec, s[i][j], ec.G, -e[i][j], pubk_rings[i][j])
             R = point2octets(ec, T, True)
             if j != len(pubk_rings[i])-1:
-                e[i][j+1] = bits2int(ec, borromean_hash(m, R, i, j+1))
+                e[i][j+1] = bits2int(ec, _borromean_hash(m, R, i, j+1))
                 assert e[i][j+1] != 0, "invalid sig: how did you do that?!?"
             else:
                 e0bytes += R
-    e0_prime = sha256(e0bytes).digest()
+    e0_prime = hf(e0bytes).digest()
     return e0_prime == e0
