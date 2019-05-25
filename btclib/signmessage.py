878c0c500781bfc5127e6e232450239a098de58a
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-05-25 23:02:19 +0200

    first (partial, buggy) implementation of message compact signature scheme

---
+++ btclib/signmessage.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2019 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+""" Bitcoin address-based compact signature for messages.
+
+For message signatures, Bitcoin wallets use an address-based scheme with a
+compact 65 bytes custom signature encoding. Such a signature proves the control
+of the private key associated to a given address and, consequently, of the
+associated bitcoins (if any).
+The signature goes along with the address: at verification time public key
+recovery is used, i.e. given a message, the public key that would have created
+that signature is found and compared with the provided address.
+
+Note that in the Bitcoin protocol this compact 65 bytes signature encoding is
+only used for messages: for transactions Bitcoin uses DER encoding instead,
+resulting in 71 bytes signatures on average.
+
+This scheme being address-based, at signing time it must rely on a wallet
+infrastructure to access the private key corresponding to the provided address.
+For a given message and address, the ECDSA signature of
+"\x18Bitcoin Signed Message:\n" + chr(len(msg)) + msg is calculated
+(0x18 is just the length of the prefix text); this prefix manipulation avoids
+the plain signature of a possibly deceiving message.
+Finally, the resulting 64 bytes (r, s) signature is base64-encoded as
+[1 byte][r][s], where the first byte is used to convey
+information in the verification process about which of the
+recovered public keys will have to be used and
+if the corresponding address is compressed or not.
+
+Base64-encoding uses 10 digits, 26 lowercase characters,
+26 uppercase characters, '+' (plus sign), and '/' (forward slash);
+equal sign '=' is used as 65th character pad,
+a complement in the final process of encoding a message.
+
+https://bitcoin.stackexchange.com/questions/10759/how-does-the-signature-verification-feature-in-bitcoin-qt-work-without-a-public
+https://bitcoin.stackexchange.com/questions/12554/why-the-signature-is-always-65-13232-bytes-long
+https://bitcoin.stackexchange.com/questions/34135/what-is-the-strmessagemagic-good-for
+https://bitcoin.stackexchange.com/questions/36838/why-does-the-standard-bitcoin-message-signature-include-a-magic-prefix
+https://bitcoin.stackexchange.com/questions/68844/explicit-message-length-in-bitcoin-signed-message
+https://github.com/bitcoinjs/bitcoinjs-lib/blob/1079bf95c1095f7fb018f6e4757277d83b7b9d07/src/message.js#L13
+https://bitcointalk.org/index.php?topic=6428.msg536734#msg536734
+https://bitcointalk.org/index.php?topic=6428.msg550155#msg550155
+https://github.com/bitcoin/bitcoin/pull/524
+https://www.reddit.com/r/Bitcoin/comments/bgcgs2/can_bitcoin_core_0171_sign_message_from_segwit/
+https://github.com/bitcoin/bips/blob/master/bip-0137.mediawiki
+"""
+
+import base64
+from hashlib import sha256
+from typing import Tuple
+
+from .curves import secp256k1
+from .wifaddress import address_from_pubkey
+from . import dsa
+from .curve import mult
+
+def _magic_msg(msg: str) -> bytes:
+    msg = "\x18Bitcoin Signed Message:\n" + chr(len(msg)) + msg
+    return msg.encode()
+
+def sign(prvkey: int, msg: str, compressed: bool) -> Tuple[str, str]:
+
+    pubkey = mult(secp256k1, prvkey)
+    address = address_from_pubkey(pubkey, compressed, b'\x00')
+
+    msg = _magic_msg(msg)
+    sig = dsa.sign(secp256k1, sha256, msg, prvkey)
+
+    pubkeys = dsa.pubkey_recovery(secp256k1, sha256, msg, sig)
+    for i in range(len(pubkeys)):
+        if pubkeys[i] == pubkey:
+            nV = 27 + i
+            if compressed:
+                nV += 4
+            sig = sig[0].to_bytes(32, 'big') + sig[1].to_bytes(32, 'big')
+            return address, base64.b64encode(bytes([nV]) + sig)
+
+    # it should 
+    raise ValueError("Public key could not be recovered")
+
+def verify(address: bytes, sig: bytes, msg: str) -> bool:
+    """Verify Bitcoin compact signature for a given address/message pair."""
+
+    # try/except wrapper for the Errors raised by _verify
+    try:
+        return _verify(address, sig, msg)
+    except Exception:
+        return False
+
+
+def _verify(address: bytes, sig: bytes, msg: str) -> bool:
+    # Private function for test/dev purposes
+    # It raises Errors, while verify should always return True or False
+
+    sig = base64.b64decode(sig)
+    if len(sig) != 65:
+        raise ValueError(f"Wrong encoding length: {len(sig)} instead of 65")
+
+    nV = int.from_bytes(sig[0:1], 'big')
+    #if nV < 27 or nV > 34: return False
+    # i selects which key is recovered
+    i = (nV - 27) & 3
+    compressed = (nV - 27) & 4 != 0
+
+    r = int.from_bytes(sig[1:33], 'big')
+    s = int.from_bytes(sig[33:], 'big')
+    sig = r, s
+    pubkeys = dsa.pubkey_recovery(secp256k1, sha256, _magic_msg(msg), sig)
+
+    return address == address_from_pubkey(pubkeys[i], compressed, b'\x00')
---
+++ tests/test_signmessage.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2019 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+import unittest
+
+from .signmessage import sign, verify
+from .wifaddress import prvkey_from_wif
+
+
+class TestSignMessage(unittest.TestCase):
+    def test_signmessage(self):
+        msg = "test message"
+
+        wif = b'L41XHGJA5QX43QRG3FEwPbqD5BYvy6WxUxqAMM9oQdHJ5FcRHcGk'
+        prvkey, compressed = prvkey_from_wif(wif)
+        self.assertEqual(prvkey, 0xCA978112CA1BBDCAFAC231B39A23DC4DA786EFF8147C4E72B9807785AFEE48BB)
+        self.assertTrue(compressed)
+        mysig = sign(prvkey, msg, compressed)
+        # auto-consistency check
+        self.assertTrue(verify(mysig[0], mysig[1], msg))
+        address = b'14dD6ygPi5WXdwwBTt1FBZK3aD8uDem1FY'
+        self.assertEqual(mysig[0], address)
+        sig = "IPn9bbEdNUp6+bneZqE2YJbq9Hv5aNILq9E5eZoMSF3/fBX4zjeIN6fpXfGSGPrZyKfHQ/c/kTSP+NIwmyTzMfk="
+        #self.assertEqual(mysig[1], sig)
+
+        wif = b'5KMWWy2d3Mjc8LojNoj8Lcz9B1aWu8bRofUgGwQk959Dw5h2iyw'
+        prvkey, compressed = prvkey_from_wif(wif)
+        self.assertEqual(prvkey, 0xCA978112CA1BBDCAFAC231B39A23DC4DA786EFF8147C4E72B9807785AFEE48BB)
+        self.assertFalse(compressed)
+        mysig = sign(prvkey, msg, compressed)
+        # auto-consistency check
+        self.assertTrue(verify(mysig[0], mysig[1], msg))
+        address = b'1HUBHMij46Hae75JPdWjeZ5Q7KaL7EFRSD'
+        self.assertEqual(mysig[0], address)
+        sig = "G0k+Nt1u5boTTUfLyj6x1T5flg1v9rUKGlhs/jPApaTWLHf3GVdAIOIHip6sVwXEuzQGPWIlS0VT+yryXiDaavw="
+        #self.assertEqual(mysig[1], sig)
+
+        wif = b'Ky1XfDK2v6wHPazA6ECaD8UctEoShXdchgABjpU9GWGZDxVRDBMJ'
+        prvkey, compressed = prvkey_from_wif(wif)
+        self.assertEqual(prvkey, 0x35687eed35e44235053dce4c65dc23b11327ecee9acc51c90651e7072047f886)
+        self.assertTrue(compressed)
+        mysig = sign(prvkey, msg, compressed)
+        # auto-consistency check
+        self.assertTrue(verify(mysig[0], mysig[1], msg))
+        address = b'1DAag8qiPLHh6hMFVu9qJQm9ro1HtwuyK5'
+        self.assertEqual(mysig[0], address)
+        sig = ""
+        #self.assertEqual(mysig[1], sig)
+
+        wif = b'5JDopdKaxz5bXVYXcAnfno6oeSL8dpipxtU1AhfKe3Z58X48srn'
+        prvkey, compressed = prvkey_from_wif(wif)
+        self.assertEqual(prvkey, 0x35687eed35e44235053dce4c65dc23b11327ecee9acc51c90651e7072047f886)
+        self.assertFalse(compressed)
+        mysig = sign(prvkey, msg, compressed)
+        # auto-consistency check
+        self.assertTrue(verify(mysig[0], mysig[1], msg))
+        address = b'19f7adDYqhHSJm2v7igFWZAqxXHj1vUa3T'
+        self.assertEqual(mysig[0], address)
+        sig = ""
+        #self.assertEqual(mysig[1], sig)
+
+    def test_verifymsgsig(self):
+        msg = "Hello, world!"
+        address = b'1FEz167JCVgBvhJBahpzmrsTNewhiwgWVG'
+        sig = "G+WptuOvPCSswt/Ncm1upO4lPSCWbS2cpKariPmHvxX5eOJwgqmdEExMTKvaR0S3f1TXwggLn/m4CbI2jv0SCuM="
+        self.assertTrue(verify(address, sig, msg))
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    unittest.main()
