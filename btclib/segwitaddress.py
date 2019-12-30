bba6a30b470235b7de2c7c15cf17295ad3813219
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-12-30 16:30:28 +0100

    removed rudundant copyright

--- btclib/segwitaddress.py
+++ btclib/segwitaddress.py
@@ -26,14 +26,6 @@
 # No part of btclib including this file, may be copied, modified, propagated,
 # or distributed except according to the terms contained in the LICENSE file.
 
-# Copyright (C) 2019-2020 The btclib developers
-#
-# This file is part of btclib. It is subject to the license terms in the
-# LICENSE file found in the top-level directory of this distribution.
-#
-# No part of btclib including this file, may be copied, modified, propagated,
-# or distributed except according to the terms contained in the LICENSE file.
-
 
 """SegWit address implementation.
 
