f572ed6e2ccc9d2b07eebf14511f4749a606dccf
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2020-01-02 01:52:50 +0100

    updated copyright

--- btclib/__init__.py
+++ btclib/__init__.py
@@ -1,5 +1,5 @@
 name = "btclib"
 __version__ = "2019.11.27"
 __author__ = "The btclib developers"
-__copyright__ = "Copyright (C) 2017-2019 The btclib developers"
+__copyright__ = "Copyright (C) 2017-2020 The btclib developers"
 __license__ = "MIT License"
