9071d352c3b5c33c96df8d9c57cb2065a6d1a64a
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2020-01-18 10:40:54 +0100

    added monetary amount/value conversion

---
+++ btclib/amount.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017-2020 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"""Proper handling of monetary amounts.
+
+Monetary amounts are expressed as number of satoshis in the Bitcoin Core
+client and this python library (e.g. 1 BTC is 100,000,000). Instead,
+monetary values are express as double-precision floats in the JSON API
+(e.g. 1 BTC is 1.00000000).
+
+One needs to be aware of possible floating-point conversion issues:
+as example, with floats 1.1 + 2.2 != 3.3;
+algebra with bitcoin amounts should always be performed using satoshis.
+
+The provided functions properly handle conversion between satoshi amounts
+and float monetary values.
+"""
+
+def sat_from_float(value: float) -> int:
+    """Return the satoshi amount equivalent of a float monetary value."""
+    return int(round(value * 1e8))
+
+def float_from_sat(amount: int) -> float:
+    """Return the float monetary value equivalent of a satoshi amount."""
+    return float(amount / 1e8)
---
+++ tests/test_amount.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017-2020 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+import unittest
+
+from btclib import amount
+
+class TestAmount(unittest.TestCase):
+
+    def test_conversion(self):
+        v1 = 1.1
+        v2 = 2.2
+        vtot = v1+v2
+        self.assertNotEqual(vtot, 3.3)     # _NOT_ equal !!
+        s1 = amount.sat_from_float(v1)
+        s2 = amount.sat_from_float(v2)
+        stot = s1+s2
+        self.assertEqual(stot, 330000000)
+        vtot = amount.float_from_sat(stot)
+        self.assertEqual(vtot, 3.3)        # equal !!
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    unittest.main()
