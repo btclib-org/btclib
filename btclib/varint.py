9058fed4eb6248a703425bf765be89524ee6f183
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2020-01-18 12:44:54 +0100

    added varint encoding/decoding

---
+++ btclib/varint.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017-2020 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"""Varint encoding and decoding functions.
+
+A varint (variable integer) is variable-length quantity that uses an
+arbitrary number of binary octets (eight-bit bytes) to represent an
+arbitrarily large integer.
+It is usually a base-128 (7 bits) representation of an unsigned integer
+with the addition of the eighth bit to mark continuation of bytes;
+it is used to save additional space for a resource constrained system.
+
+This is the slightly different Bitcoin implementation, used in transaction
+data to indicate the number of upcoming fields or the length of the
+upcoming field.
+
+Up to 0xfc, a varint is just 1 byte; however, if the integer is greater than
+0xfc, then it is expanded as [1 byte prefix][number]:
+
+* prefix 0xfd markes the next two bytes as the number;
+* prefix 0xfe markes the next four bytes as the number;
+* prefix 0xff markes the next eight bytes as the number.
+"""
+
+from io import BytesIO
+
+from .utils import Octets
+
+def decode(varint: Octets) -> int:
+    '''Return the integer represented by the varint stream.'''
+
+    if isinstance(varint, str):
+        varint = bytes.fromhex(varint)
+
+    s = BytesIO(varint)
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd marks the next two bytes as the number
+        return int.from_bytes(s.read(2), byteorder='little')
+    elif i == 0xfe:
+        # 0xfe marks the next four bytes as the number
+        return int.from_bytes(s.read(4), byteorder='little')
+    elif i == 0xff:
+        # 0xff marks the next eight bytes as the number
+        return int.from_bytes(s.read(8), byteorder='little')
+    else:
+        # anything else is just the integer
+        return i
+
+
+def encode(i: int) -> bytes:
+    '''Return the varint bytes encoding on an integer.'''
+    if i <= 0xfc:                  # 1 byte
+        return bytes([i])
+    elif i <= 0xffff:              # 2 bytes
+        return b'\xfd' + i.to_bytes(2, byteorder='little')
+    elif i <= 0xffffffff:          # 4 bytes
+        return b'\xfe' + i.to_bytes(4, byteorder='little')
+    elif i <= 0xffffffffffffffff:  # 8 bytes
+        return b'\xff' + i.to_bytes(8, byteorder='little')
+    else:
+        raise ValueError(f'integer too large ({hex(i)}) for varint encoding')
---
+++ tests/test_varint.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017-2020 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+import unittest
+
+from btclib import varint
+
+class TestVarInt(unittest.TestCase):
+
+    def test_conversion(self):
+
+        i = 0xfc
+        b = varint.encode(i)
+        self.assertEqual(len(b), 1)
+        self.assertEqual(varint.decode(b), i)
+
+        i = 0xfd
+        b = varint.encode(i)
+        self.assertEqual(len(b), 3)
+        self.assertEqual(varint.decode(b), i)
+
+        i = 0xffff
+        b = varint.encode(i)
+        self.assertEqual(len(b), 3)
+        self.assertEqual(varint.decode(b), i)
+
+        i = 0xffffffff
+        b = varint.encode(i)
+        self.assertEqual(len(b), 5)
+        self.assertEqual(varint.decode(b), i)
+
+        i = 0xffffffffffffffff
+        b = varint.encode(i)
+        self.assertEqual(len(b), 9)
+        self.assertEqual(varint.decode(b), i)
+
+        # integer too large (0x10000000000000000) for varint encoding
+        i += 1
+        self.assertRaises(ValueError, varint.encode, i)
+        #varint.encode(i)
+
+        self.assertEqual(varint.decode('6a'), 106)
+        self.assertEqual(varint.decode('fd2602'), 550)
+        self.assertEqual(varint.decode('fe703a0f00'), 998000)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    unittest.main()
