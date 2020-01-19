cd988b64a7058a49f8127bb3d918fcd208c58e06
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2020-01-19 10:06:37 +0100

    increased test coverage

--- btclib/scriptpubkey.py
+++ btclib/scriptpubkey.py
@@ -20,106 +20,113 @@ from .utils import Octets
 
 def nulldata_scriptPubKey(data: Octets) -> List[Token]:
     """Return the nulldata scriptPubKey with the provided data."""
-    if isinstance(data, bytes):
-        data = data.hex()
-    if len(data) > 80:
-        msg = f"Invalid data lenght ({len(data)//2} bytes) "
+    if isinstance(data, str):  # hex string
+        data = data.strip()
+        data = bytes.fromhex(data)
+    if len(data) > 40:
+        msg = f"Invalid data lenght ({len(data)} bytes) "
         msg += "for nulldata scriptPubKey"
         raise ValueError(msg)
-    return ['OP_RETURN', data]
+    return ['OP_RETURN', data.hex()]
 
 
 def p2pk_scriptPubKey(pubkey: Octets) -> List[Token]:
     """Return the p2pk scriptPubKey of the provided pubkey."""
-    if isinstance(pubkey, bytes):
-        pubkey = pubkey.hex()
-    if len(pubkey) not in (66, 130):
-        msg = f"Invalid pubkey lenght ({len(pubkey)//2} bytes) "
+    if isinstance(pubkey, str):  # hex string
+        pubkey = pubkey.strip()
+        pubkey = bytes.fromhex(pubkey)
+    if len(pubkey) not in (33, 65):
+        msg = f"Invalid pubkey lenght ({len(pubkey)} bytes) "
         msg += "for p2pk scriptPubKey"
         raise ValueError(msg)
-    return [pubkey, 'OP_CHECKSIG']
+    return [pubkey.hex(), 'OP_CHECKSIG']
 
 
 def multisig_scriptPubKey(m: int, pubkeys: Iterable[Octets]) -> List[Token]:
     """Return the m-of-n multi-sig scriptPubKey of the provided pubkeys."""
 
     if m<1 or m>16:
-        raise ValueError("Invalid m ({m})")
+        raise ValueError(f"Invalid m ({m})")
         # raise ValueError("Impossible m-of-n ({m}-of-{n})")
 
     scriptPubKey : List[Token] = [m]
     for pubkey in pubkeys:
-        if isinstance(pubkey, bytes):
-            pubkey = pubkey.hex()
-        if len(pubkey) not in (66, 130):
-            msg = f"Invalid pubkey lenght ({len(pubkey)//2} bytes) "
+        if isinstance(pubkey, str):  # hex string
+            pubkey = pubkey.strip()
+            pubkey = bytes.fromhex(pubkey)
+        if len(pubkey) not in (33, 65):
+            msg = f"Invalid pubkey lenght ({len(pubkey)} bytes) "
             msg += "for m-of-n multi-sig scriptPubKey"
             raise ValueError(msg)
-        scriptPubKey.append(pubkey)
+        scriptPubKey.append(pubkey.hex())
 
     # FIXME: handle script max length
     # FIXME: enable lexicographic key sorting
     n = len(scriptPubKey)-1
     if n<1 or n>16:
-        raise ValueError("Invalid n ({n})")
+        raise ValueError(f"Invalid n ({n})")
     if m>n:
-        raise ValueError("Impossible m-of-n ({m}-of-{n})")
+        raise ValueError(f"Impossible m-of-n ({m}-of-{n})")
     scriptPubKey.append(n)
     scriptPubKey.append('OP_CHECKMULTISIGVERIFY')
     return scriptPubKey
 
 
-def p2pkh_scriptPubKey(h160: Octets) -> List[Token]:
+def p2pkh_scriptPubKey(pubkey_h160: Octets) -> List[Token]:
     """Return the p2pkh scriptPubKey of the provided HASH160 pubkey-hash."""
-    if isinstance(h160, bytes):
-        h160 = h160.hex()
-    if len(h160) != 40:
-        msg = f"Invalid pubkey-hash lenght ({len(h160)//2} bytes) "
+    if isinstance(pubkey_h160, str):  # hex string
+        pubkey_h160 = pubkey_h160.strip()
+        pubkey_h160 = bytes.fromhex(pubkey_h160)
+    if len(pubkey_h160) != 20:
+        msg = f"Invalid pubkey-hash lenght ({len(pubkey_h160)} bytes) "
         msg += "for p2pkh scriptPubKey"
         raise ValueError(msg)
-    return ['OP_DUP', 'OP_HASH160', h160, 'OP_EQUALVERIFY', 'OP_CHECKSIG']
+    return ['OP_DUP', 'OP_HASH160', pubkey_h160.hex(), 'OP_EQUALVERIFY', 'OP_CHECKSIG']
 
 
-def p2sh_scriptPubKey(h160: Octets) -> List[Token]:
+def p2sh_scriptPubKey(script_h160: Octets) -> List[Token]:
     """Return the p2sh scriptPubKey of the provided HASH160 script-hash."""
-    if isinstance(h160, bytes):
-        h160 = h160.hex()
-    if len(h160) != 40:
-        msg = f"Invalid script-hash lenght ({len(h160)//2} bytes) "
+    if isinstance(script_h160, str):  # hex string
+        script_h160 = script_h160.strip()
+        script_h160 = bytes.fromhex(script_h160)
+    if len(script_h160) != 20:
+        msg = f"Invalid script-hash lenght ({len(script_h160)} bytes) "
         msg += "for p2sh scriptPubKey"
         raise ValueError(msg)
-    return ['OP_HASH160', h160, 'OP_EQUAL']
+    return ['OP_HASH160', script_h160.hex(), 'OP_EQUAL']
 
 
-def p2wpkh_scriptPubKey(h160: Octets) -> List[Token]:
+def p2wpkh_scriptPubKey(pubkey_h160: Octets) -> List[Token]:
     """Return the p2wpkh scriptPubKey of the provided HASH160 pubkey-hash.
     
-    For P2WPKH the witness program must be the HASH160 20-byte pubkey-hash;
+    For P2WPKH, the witness program must be the HASH160 20-byte pubkey-hash;
     the scriptPubkey is the witness version 0 followed by the canonical push
     of the witness program (program lenght + program),
     that is 0x0014{20-byte key-hash}
     """
-    if isinstance(h160, bytes):
-        h160 = h160.hex()
-    if len(h160) != 40:
-        msg = f"Invalid witness program lenght ({len(h160)//2} bytes) "
+    if isinstance(pubkey_h160, str):  # hex string
+        pubkey_h160 = pubkey_h160.strip()
+        pubkey_h160 = bytes.fromhex(pubkey_h160)
+    if len(pubkey_h160) != 20:
+        msg = f"Invalid witness program lenght ({len(pubkey_h160)} bytes) "
         msg += "for p2wpkh scriptPubKey"
         raise ValueError(msg)
-    return [0, h160]
+    return [0, pubkey_h160.hex()]
 
 
-def p2wsh_scriptPubKey(h256: Octets) -> List[Token]:
-    """Return the p2wsh scriptPubKey of the SHA256 script-hash.
+def p2wsh_scriptPubKey(script_h160: Octets) -> List[Token]:
+    """Return the p2wsh scriptPubKey of the provided SHA256 script-hash.
     
-    For P2WSH the witness program must be the SHA256 32-byte script-hash;
+    For P2WSH, the witness program must be the SHA256 32-byte script-hash;
     the scriptPubkey is the witness version 0 followed by the canonical push
     of the witness program (program lenght + program),
     that is 0x0020{32-byte script-hash}
     """
-    if isinstance(h256, bytes):
-        h256 = h256.hex()
-    if len(h256) != 64:
-        msg = f"Invalid witness program lenght ({len(h256)//2} bytes) "
+    if isinstance(script_h160, str):  # hex string
+        script_h160 = script_h160.strip()
+        script_h160 = bytes.fromhex(script_h160)
+    if len(script_h160) != 32:
+        msg = f"Invalid witness program lenght ({len(script_h160)} bytes) "
         msg += "for p2wsh scriptPubKey"
         raise ValueError(msg)
-    return [0, h256]
+    return [0, script_h160.hex()]
--- tests/test_scriptpubkey.py
+++ tests/test_scriptpubkey.py
@@ -22,24 +22,24 @@ class TestScriptPubKey(unittest.TestCase):
 
     def test_standards(self):
 
-        data = "time-stamped data".encode()
         # OP_RETURN
+        data = "time-stamped data".encode().hex()
         # script = ['OP_RETURN', data.hex()]
         script = nulldata_scriptPubKey(data)
         script_bytes = serialize(script)
         script2 = parse(script_bytes)
         self.assertEqual(script, script2)
 
-        pubkey = "03a1af804ac108a8a51782198c2d034b28bf90c8803f5a53f76276fa69a4eae77f"
         # p2pk
+        pubkey = "03a1af804ac108a8a51782198c2d034b28bf90c8803f5a53f76276fa69a4eae77f"
         #script = [pubkey, 'OP_CHECKSIG']
         script = p2pk_scriptPubKey(pubkey)
         script_bytes = serialize(script)
         script2 = parse(script_bytes)
         self.assertEqual(script, script2)
 
-        pubKey2 = "02530c548d402670b13ad8887ff99c294e67fc18097d236d57880c69261b42def7"
         # multi-sig
+        pubKey2 = "02530c548d402670b13ad8887ff99c294e67fc18097d236d57880c69261b42def7"
         # script = [1, pubkey, pubKey2, 2, 'OP_CHECKMULTISIGVERIFY']
         script = multisig_scriptPubKey(1, (pubkey, pubKey2))
         script_bytes = serialize(script)
@@ -47,15 +47,15 @@ class TestScriptPubKey(unittest.TestCase):
         self.assertEqual(script, script2)
 
         # p2pkh
-        pubkey_hash = h160(pubkey)
+        pubkey_hash = h160(pubkey).hex()
         # script = ['OP_DUP', 'OP_HASH160', pubkey_hash.hex(), 'OP_EQUALVERIFY', 'OP_CHECKSIG']
         script = p2pkh_scriptPubKey(pubkey_hash)
         script_bytes = serialize(script)
         script2 = parse(script_bytes)
         self.assertEqual(script, script2)
 
-        # p2pkh-p2sh
-        redeem_script_hash = h160(script_bytes)
+        # p2sh (p2pkh-p2sh)
+        redeem_script_hash = h160(script_bytes).hex()
         # script = ['OP_HASH160', redeem_script_hash.hex(), 'OP_EQUAL']
         script = p2sh_scriptPubKey(redeem_script_hash)
         script_bytes = serialize(script)
@@ -66,7 +66,7 @@ class TestScriptPubKey(unittest.TestCase):
         # script = [0, pubkey_hash.hex()]
         script = p2wpkh_scriptPubKey(pubkey_hash)
         script_bytes = serialize(script)
-        self.assertEqual(script_bytes.hex(), "0014"+pubkey_hash.hex())
+        self.assertEqual(script_bytes.hex(), "0014"+pubkey_hash)
         script2 = parse(script_bytes)
         self.assertEqual(script, script2)
 
@@ -75,11 +75,60 @@ class TestScriptPubKey(unittest.TestCase):
         witness_script_bytes = serialize(witness_script)
         witness_script_hash = _sha256(witness_script_bytes)
         # script = [0, witness_script_hash.hex()]
-        script = p2wsh_scriptPubKey(witness_script_hash)
+        script = p2wsh_scriptPubKey(witness_script_hash.hex())
         script_bytes = serialize(script)
         self.assertEqual(script_bytes.hex(), "0020"+witness_script_hash.hex())
         script2 = parse(script_bytes)
         self.assertEqual(script, script2)
+
+    def test_exceptions(self):
+
+        # Invalid data lenght (41 bytes) for nulldata scriptPubKey
+        data = '00'*41
+        self.assertRaises(ValueError, nulldata_scriptPubKey, data)
+        #nulldata_scriptPubKey(data)
+
+        # Invalid pubkey lenght (34 bytes) for p2pk scriptPubKey
+        pubkey = "03a1af804ac108a8a51782198c2d034b28bf90c8803f5a53f76276fa69a4eae77f"
+        self.assertRaises(ValueError, p2pk_scriptPubKey, pubkey+"00")
+        # p2pk_scriptPubKey(pubkey+"00")
+
+        # Invalid m (0)
+        pubKey2 = "02530c548d402670b13ad8887ff99c294e67fc18097d236d57880c69261b42def7"
+        self.assertRaises(ValueError, multisig_scriptPubKey, 0, (pubkey, pubKey2))
+        # multisig_scriptPubKey(0, (pubkey, pubKey2))
+
+        # Invalid pubkey lenght (34 bytes) for m-of-n multi-sig scriptPubKey
+        pubKey2 = "02530c548d402670b13ad8887ff99c294e67fc18097d236d57880c69261b42def7"
+        self.assertRaises(ValueError, multisig_scriptPubKey, 1, (pubkey+"00", pubKey2))
+        #multisig_scriptPubKey(1, (pubkey+"00", pubKey2))
+
+        # Invalid n (17)
+        pubKey2 = "02530c548d402670b13ad8887ff99c294e67fc18097d236d57880c69261b42def7"
+        self.assertRaises(ValueError, multisig_scriptPubKey, 3, [pubkey]*17)
+        # multisig_scriptPubKey(3, [pubkey]*17)
+
+        # Impossible m-of-n (3-of-2)
+        pubKey2 = "02530c548d402670b13ad8887ff99c294e67fc18097d236d57880c69261b42def7"
+        self.assertRaises(ValueError, multisig_scriptPubKey, 3, (pubkey, pubKey2))
+        # multisig_scriptPubKey(3, (pubkey, pubKey2))
+
+        # Invalid pubkey-hash lenght (11 bytes) for p2pkh scriptPubKey
+        pubkey_hash = h160(pubkey).hex()
+        self.assertRaises(ValueError, p2pkh_scriptPubKey, "00"*11)
+        # p2pkh_scriptPubKey("00"*11)
+
+        # Invalid script-hash lenght (21 bytes) for p2sh scriptPubKey
+        self.assertRaises(ValueError, p2sh_scriptPubKey, "00"*21)
+        # p2sh_scriptPubKey("00"*21)
+
+        # Invalid witness program lenght (11 bytes) for p2wpkh scriptPubKey
+        self.assertRaises(ValueError, p2wpkh_scriptPubKey, "00"*11)
+        # p2wpkh_scriptPubKey("00"*11)
+
+        # Invalid witness program lenght (33 bytes) for p2wsh scriptPubKey
+        self.assertRaises(ValueError, p2wsh_scriptPubKey, "00"*33)
+        # p2wsh_scriptPubKey("00"*33)
 
 
 if __name__ == "__main__":
