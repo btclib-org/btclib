b7c2eec9266f8afaf8b973c6f784c0da36351d65
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2020-04-05 16:50:15 +0200

    cleaned up code

--- btclib/to_prvkey.py
+++ btclib/to_prvkey.py
@@ -31,35 +31,29 @@ def to_prvkey_int(prvkey: Union[int, bytes, str, bip32.XkeyDict], ec: Curve = se
 
     if isinstance(prvkey, int):
         q = prvkey
+    elif isinstance(prvkey, dict):
+        q, _, _ = prvkeytuple_from_xprv(prvkey)
+        # it has been already validated as 0 < q < n
+        return q
     else:
-        if isinstance(prvkey, dict):
-            try:
-                q, _, _ = prvkeytuple_from_xprv(prvkey)
-            except Exception:
-                pass
-            else:
-                # it has been already validated as 0 < q < n
-                return q
+        try:
+            q, _, _ = prvkeytuple_from_xprv(prvkey)
+        except Exception:
+            pass
         else:
-            try:
-                q, _, _ = prvkeytuple_from_xprv(prvkey)
-            except Exception:
-                pass
-            else:
-                # it has been already validated as 0 < q < n
-                return q
+            # it has been already validated as 0 < q < n
+            return q
 
-            try:
-                q, _, _ = prvkeytuple_from_wif(prvkey)
-            except Exception:
-                pass
-            else:
-                # it has been already validated as 0 < q < n
-                return q
+        try:
+            q, _, _ = prvkeytuple_from_wif(prvkey)
+        except Exception:
+            pass
+        else:
+            # it has been already validated as 0 < q < n
+            return q
 
-            if isinstance(prvkey, str) or isinstance(prvkey, bytes):
-                prvkey = bytes_from_octets(prvkey, ec.nsize)
-                q = int.from_bytes(prvkey, 'big')
+        prvkey = bytes_from_octets(prvkey, ec.nsize)
+        q = int.from_bytes(prvkey, 'big')
 
     if not 0 < q < ec.n:
         raise ValueError(f"Private key {hex(q).upper()} not in [1, n-1]")
