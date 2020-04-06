e94fdcf344bbe6b11bda087af1bca47bd9df3074
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2020-04-06 02:40:38 +0200

    cleaned up code

--- btclib/to_prvkey.py
+++ btclib/to_prvkey.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2017-2020 The btclib developers
+# Copyright (C) 2020 The btclib developers
 #
 # This file is part of btclib. It is subject to the license terms in the
 # LICENSE file found in the top-level directory of this distribution.
@@ -17,8 +17,9 @@ from .curve import Curve
 from .curves import secp256k1
 from .utils import bytes_from_octets
 
+Key = Union[int, bytes, str, bip32.XkeyDict]
 
-def to_prvkey_int(prvkey: Union[int, bytes, str, bip32.XkeyDict], ec: Curve = secp256k1) -> int:
+def to_prvkey_int(prvkey: Key, ec: Curve = secp256k1) -> int:
     """Return a verified-as-valid private key integer.
     
     It supports:
@@ -26,35 +27,34 @@ def to_prvkey_int(prvkey: Union[int, bytes, str, bip32.XkeyDict], ec: Curve = se
     - WIF (bytes or string)
     - BIP32 extended keys (bytes, string, or XkeyDict)
     - Octets (bytes or hex-string)
-    - native tuple
+    - native integer
     """
 
     if isinstance(prvkey, int):
         q = prvkey
+    elif isinstance(prvkey, dict):
+        q, _, _ = prvkeytuple_from_xprv(prvkey)
+        # it has been already validated as 0 < q < n
+        return q
     else:
-        if isinstance(prvkey, dict):
+        try:
             q, _, _ = prvkeytuple_from_xprv(prvkey)
+        except Exception:
+            pass
+        else:
             # it has been already validated as 0 < q < n
             return q
+
+        try:
+            q, _, _ = prvkeytuple_from_wif(prvkey)
+        except Exception:
+            pass
         else:
-            try:
-                q, _, _ = prvkeytuple_from_xprv(prvkey)
-            except Exception:
-                pass
-            else:
-                # it has been already validated as 0 < q < n
-                return q
+            # it has been already validated as 0 < q < n
+            return q
 
-            try:
-                q, _, _ = prvkeytuple_from_wif(prvkey)
-            except Exception:
-                pass
-            else:
-                # it has been already validated as 0 < q < n
-                return q
-
-            prvkey = bytes_from_octets(prvkey, ec.nsize)
-            q = int.from_bytes(prvkey, 'big')
+        prvkey = bytes_from_octets(prvkey, ec.nsize)
+        q = int.from_bytes(prvkey, 'big')
 
     if not 0 < q < ec.n:
         raise ValueError(f"Private key {hex(q).upper()} not in [1, n-1]")
