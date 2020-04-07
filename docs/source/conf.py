5e57a741907eb9b7b827e700e59be824398e3e5d
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2020-04-07 09:13:38 +0200

    getting ready for new release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,12 +6,13 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import sys
+from os import path
+from typing import List
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-from os import path
-import sys
 sys.path.insert(0, path.abspath('../../'))
 
 
@@ -40,7 +41,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,6 +54,6 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path: List[str] = []
 
 master_doc = 'index'
