2239fc7b12f9fced962e357d51ed10f6df916695
Ferdinando Ametrano <ferdinando@ametrano.net>
2020-05-18 02:15:33 +0200

    reduces complexity

--- btclib/scriptpubkey.py
+++ btclib/scriptpubkey.py
@@ -93,6 +93,66 @@ def scriptPubKey_from_payload(
 Payloads = Union[bytes, List[bytes]]
 
 
+def payload_from_nulldata_scriptPubKey(script: Script) -> Tuple[str, Payloads, int]:
+    if isinstance(script, list):
+        s = encode(script)
+    else:
+        s = bytes_from_octets(script)
+
+    length = len(s)
+
+    # nulldata [OP_RETURN, data]
+    zeroone = int(length > 77)
+    if s[1 + zeroone] != length - 2 - zeroone:
+        raise ValueError(
+            f"Wrong data lenght ({s[1+zeroone]}) in "
+            f"{length}-bytes nulldata script: it should "
+            f"have been {length-2-zeroone}: {decode(s)}"
+        )
+    if length < 77:
+        # OP_RETURN, data length, data up to 74 bytes max
+        # 0x6A{1 byte data-length}{data (0-74 bytes)}
+        return "nulldata", s[2:], 0
+    if length > 77:
+        # OP_RETURN, OP_PUSHDATA1, data length, data min 75 bytes up to 80
+        # 0x6A4C{1-byte data-length}{data (75-80 bytes)}
+        if s[1] != 0x4C:
+            raise ValueError(
+                f"Missing OP_PUSHDATA1 (0x4c) in "
+                f"{length}-bytes nulldata script, "
+                f"got {hex(s[1])} instead: {decode(s)}"
+            )
+        return "nulldata", s[3:], 0
+    raise ValueError("Invalid 77 bytes OP_RETURN script length")
+
+
+def payload_from_pms_scriptPubKey(script: Script) -> Tuple[str, Payloads, int]:
+    if isinstance(script, list):
+        s = encode(script)
+    else:
+        s = bytes_from_octets(script)
+
+    # p2ms [m, pubkeys, n, OP_CHECKMULTISIG]
+    script = decode(s)
+    m = int(script[0])
+    if m < 1 or m > 16:
+        raise ValueError(f"Invalid m ({m}) in {m}-of-n multisignature")
+    n = len(script) - 3
+    if n < m or n > 16:
+        raise ValueError(f"Invalid n ({n}) in {m}-of-{n} multisignature")
+    if n != int(script[-2]):
+        errmsg = f"Keys ({n}) / n ({int(script[-2])}) mismatch "
+        errmsg += "in m-of-n multisignature"
+        raise ValueError(errmsg)
+    keys: List[bytes] = []
+    for pk in script[1:-2]:
+        if isinstance(pk, int):
+            raise ValueError("Invalid key in p2ms")
+        key = bytes_from_octets(pk, (33, 65))
+        keys.append(key)
+    return "p2ms", keys, m
+
+
 def payload_from_scriptPubKey(script: Script) -> Tuple[str, Payloads, int]:
     "Return (scriptPubKey type, payload, m) from the input script."
 
@@ -102,77 +162,34 @@ def payload_from_scriptPubKey(script: Script) -> Tuple[str, Payloads, int]:
         s = bytes_from_octets(script)
 
     length = len(s)
-    # p2pk
+
+    # p2pk [pubkey, OP_CHECKSIG]
+    # 0x41{65-byte pubkey}AC or 0x21{33-byte pubkey}AC
     if length == s[0] + 2 and s[0] in (0x41, 0x21) and s[-1] == 0xAC:
-        # p2pk [pubkey, OP_CHECKSIG]
-        # 0x41{65-byte pubkey}AC or 0x21{33-byte pubkey}AC
         return "p2pk", s[1:-1], 0
-    # p2ms
+    # p2ms [m, pubkeys, n, OP_CHECKMULTISIG]
     elif s[-1] == 0xAE:
-        # p2ms [m, pubkeys, n, OP_CHECKMULTISIG]
-        script = decode(s)
-        m = int(script[0])
-        if m < 1 or m > 16:
-            raise ValueError(f"Invalid m ({m}) in {m}-of-n multisignature")
-        n = len(script) - 3
-        if n < m or n > 16:
-            raise ValueError(f"Invalid n ({n}) in {m}-of-{n} multisignature")
-        if n != int(script[-2]):
-            errmsg = f"Keys ({n}) / n ({int(script[-2])}) mismatch "
-            errmsg += "in m-of-n multisignature"
-            raise ValueError(errmsg)
-        keys: List[bytes] = []
-        for pk in script[1:-2]:
-            if isinstance(pk, int):
-                raise ValueError("Invalid key in p2ms")
-            key = bytes_from_octets(pk, (33, 65))
-            keys.append(key)
-        return "p2ms", keys, m
-    # nulldata
+        return payload_from_pms_scriptPubKey(script)
+    # nulldata [OP_RETURN, data]
     elif length <= 83 and s[0] == 0x6A:
-        # nulldata [OP_RETURN, data]
-        zeroone = int(length > 77)
-        if s[1 + zeroone] != length - 2 - zeroone:
-            raise ValueError(
-                f"Wrong data lenght ({s[1+zeroone]}) in "
-                f"{length}-bytes nulldata script: it should "
-                f"have been {length-2-zeroone}: {decode(s)}"
-            )
-        if length < 77:
-            # OP_RETURN, data length, data up to 74 bytes max
-            # 0x6A{1 byte data-length}{data (0-74 bytes)}
-            return "nulldata", s[2:], 0
-        if length > 77:
-            # OP_RETURN, OP_PUSHDATA1, data length, data min 75 bytes up to 80
-            # 0x6A4C{1-byte data-length}{data (75-80 bytes)}
-            if s[1] != 0x4C:
-                raise ValueError(
-                    f"Missing OP_PUSHDATA1 (0x4c) in "
-                    f"{length}-bytes nulldata script, "
-                    f"got {hex(s[1])} instead: {decode(s)}"
-                )
-            return "nulldata", s[3:], 0
-        raise ValueError("Invalid 77 bytes OP_RETURN script length")
-    # pkh
+        return payload_from_nulldata_scriptPubKey(script)
+    # p2pkh [OP_DUP, OP_HASH160, pubkey_hash, OP_EQUALVERIFY, OP_CHECKSIG]
+    # 0x76A914{20-byte pubkey_hash}88AC
     elif length == 25 and s[:3] == b"\x76\xa9\x14" and s[-2:] == b"\x88\xac":
-        # p2pkh [OP_DUP, OP_HASH160, pubkey_hash, OP_EQUALVERIFY, OP_CHECKSIG]
-        # 0x76A914{20-byte pubkey_hash}88AC
         return "p2pkh", s[3 : length - 2], 0
-    # sh
+    # p2sh [OP_HASH160, script_hash, OP_EQUAL]
+    # 0xA914{20-byte script_hash}87
     elif length == 23 and s[:2] == b"\xa9\x14" and s[-1] == 0x87:
-        # p2sh [OP_HASH160, script_hash, OP_EQUAL]
-        # 0xA914{20-byte script_hash}87
         return "p2sh", s[2 : length - 1], 0
-    # wkh
+    # p2wpkh [0, pubkey_hash]
+    # 0x0014{20-byte pubkey_hash}
     elif length == 22 and s[:2] == b"\x00\x14":
-        # p2wpkh [0, pubkey_hash]
-        # 0x0014{20-byte pubkey_hash}
         return "p2wpkh", s[2:], 0
-    # wsh
+    # p2wsh [0, script_hash]
+    # 0x0020{32-byte script_hash}
     elif length == 34 and s[:2] == b"\x00\x20":
-        # p2wsh [0, script_hash]
-        # 0x0020{32-byte script_hash}
         return "p2wsh", s[2:], 0
+    # Unknow script
     else:
         errmsg = f"Unknown {len(s)}-bytes script"
         errmsg += f", starts with {s[:3].hex()}"
