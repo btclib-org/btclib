2f31710872ecbf949fadf07e91fc3e9194eb9507
Filippo Beretta <filippo.beretta1@studenti.unimi.it>
2020-07-28 17:59:41 +0200

    Added tests for mult_base3 and mult_fixed_window

--- btclib/tests/test_curvemult2.py
+++ btclib/tests/test_curvemult2.py
@@ -16,7 +16,7 @@ import pytest
 from btclib.alias import INFJ, Integer, JacPoint, Point
 from btclib.curve import Curve, CurveGroup, _jac_from_aff, _mult_jac
 from btclib.curvemult import mult
-from btclib.curvemult2 import _mult_jac_mont_ladder, mult_mont_ladder
+from btclib.curvemult2 import _mult_jac_mont_ladder, mult_mont_ladder, _mult_jac_base_3, _mult_jac_fixed_window
 from btclib.curves import secp256k1
 from btclib.tests.test_curves import low_card_curves
 
@@ -67,3 +67,50 @@ def test_mont_ladder():
 
             with pytest.raises(ValueError, match="negative m: -0x"):
                 _mult_jac_mont_ladder(-1, ec.GJ, ec)
+
+
+def test_mult_jac_base_3():
+    for x in range(50):
+        for ec in low_card_curves.values():
+            assert _mult_jac_base_3(0, ec.GJ, ec) == INFJ
+            assert _mult_jac_base_3(0, INFJ, ec) == INFJ
+
+            assert _mult_jac_base_3(1, INFJ, ec) == INFJ
+            assert _mult_jac_base_3(1, ec.GJ, ec) == ec.GJ
+
+            PJ = ec._add_jac(ec.GJ, ec.GJ)
+            assert PJ == _mult_jac_base_3(2, ec.GJ, ec)
+
+            PJ = _mult_jac_base_3(ec.n - 1, ec.GJ, ec)
+            assert ec._jac_equality(ec.negate(ec.GJ), PJ)
+
+            assert _mult_jac_base_3(ec.n - 1, INFJ, ec) == INFJ
+            assert ec._add_jac(PJ, ec.GJ) == INFJ
+            assert _mult_jac_base_3(ec.n, ec.GJ, ec) == INFJ
+
+            with pytest.raises(ValueError, match="negative m: -0x"):
+                _mult_jac_base_3(-1, ec.GJ, ec)
+
+
+def test_mult_jac_fixed_window():
+    for k in range(1, 10):  # Actually it makes use of w=4 or w=5, only to check
+        for x in range(50):
+            for ec in low_card_curves.values():
+                assert _mult_jac_fixed_window(0, k, ec.GJ, ec) == INFJ
+                assert _mult_jac_fixed_window(0, k, INFJ, ec) == INFJ
+
+                assert _mult_jac_fixed_window(1, k, INFJ, ec) == INFJ
+                assert _mult_jac_fixed_window(1, k, ec.GJ, ec) == ec.GJ
+
+                PJ = ec._add_jac(ec.GJ, ec.GJ)
+                assert PJ == _mult_jac_fixed_window(2, k, ec.GJ, ec)
+
+                PJ = _mult_jac_fixed_window(ec.n - 1, k, ec.GJ, ec)
+                assert ec._jac_equality(ec.negate(ec.GJ), PJ)
+
+                assert _mult_jac_fixed_window(ec.n - 1, k, INFJ, ec) == INFJ
+                assert ec._add_jac(PJ, ec.GJ) == INFJ
+                assert _mult_jac_fixed_window(ec.n, k, ec.GJ, ec) == INFJ
+
+                with pytest.raises(ValueError, match="negative m: -0x"):
+                    _mult_jac_fixed_window(-1, k, ec.GJ, ec)
