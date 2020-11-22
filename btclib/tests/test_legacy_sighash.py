ac065c3a0477fcf11047dc8c9a8f1a3924831cff
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-11-22 17:52:17 +0100

    Add legacy sighash

---
+++ btclib/tests/test_legacy_sighash.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2020 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"Tests for `btclib.sighash` module."
+
+# test vector at https://github.com/bitcoin/bips/blob/master/bip-0143.mediawiki
+from btclib import script, dsa
+from btclib.tx_in import TxIn, OutPoint
+from btclib.tx_out import TxOut
+from btclib.tx import Tx
+from btclib.sighash import get_sighash, legacy_sighash, _get_legacy_scriptCodes
+
+# from btclib.curve import mult
+# from btclib.secpoint import bytes_from_point
+# from btclib.script import deserialize
+
+import json
+import os
+
+# block 170
+def test_first_transaction():
+    transaction = Tx.deserialize(
+        "0100000001c997a5e56e104102fa209c6a852dd90660a20b2d9c352423edce25857fcd3704000000004847304402204e45e16932b8af514961a1d3a1a25fdf3f4f7732e9d624c6c61548ab5fb8cd410220181522ec8eca07de4860a4acdd12909d831cc56cbbac4622082221a8768d1d0901ffffffff0200ca9a3b00000000434104ae1a62fe09c5f51b13905f07f06b99a2f7159b2225f374cd378d71302fa28414e7aab37397f554a7df5f142c21c1b7303b8a0626f1baded5c72a704f7e6cd84cac00286bee0000000043410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3ac00000000"
+    )
+    previous_txout = TxOut(
+        value=5000000000,
+        scriptPubKey=bytes.fromhex(
+            "410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3ac"
+        ),
+    )
+    sighash = get_sighash(transaction, previous_txout, 0, 0x01)
+    assert (
+        sighash.hex()
+        == "7a05c6145f10101e9d6325494245adf1297d80f8f38d4d576d57cdba220bcb19"
+    )
+    pubkey = "0411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3"
+    signature = "304402204E45E16932B8AF514961A1D3A1A25FDF3F4F7732E9D624C6C61548AB5FB8CD410220181522EC8ECA07DE4860A4ACDD12909D831CC56CBBAC4622082221A8768D1D0901"
+    assert dsa._verify(sighash, bytes.fromhex(pubkey), bytes.fromhex(signature)[:-1])
+
+
+# 8fea2a92db2940ebce62610b162bfe0ca13229e08cb384a886a6f677e2812e52
+def test_legacy_p2pkh():
+    pubkey = "04280c8f66bf2ccaeb3f60a19ad4a06365f8bd6178aab0e709df2173df8f553366549aec336aae8742a84702b6c7c3052d89f5d76d535ec3716e72187956351613"
+    signature = "3045022100ea43c4800d1a860ec89b5273898a146cfb01d34ff4c364d24a110c480d0e3f7502201c82735577f932f1ca8e1c54bf653e0f8e74e408fe83666bc85cac4472ec950801"
+    scriptSig = [signature, pubkey]
+    previous_txout = TxOut(
+        value=1051173696,
+        scriptPubKey=script.serialize(
+            [
+                "OP_DUP",
+                "OP_HASH160",
+                "82ac30f58baf99ec9d14e6181eee076f4e27f69c",
+                "OP_EQUALVERIFY",
+                "OP_CHECKSIG",
+            ]
+        ),
+    )
+    tx = Tx(
+        1,
+        0,
+        vin=[
+            TxIn(
+                OutPoint(
+                    bytes.fromhex(
+                        "d8343a35ba951684f2969eafe833d9e6fe436557b9707ae76802875952e860fc"
+                    ),
+                    1,
+                ),
+                scriptSig,
+                0xFFFFFFFF,
+                [],
+            )
+        ],
+        vout=[
+            TxOut(
+                2017682,
+                bytes.fromhex("76a91413bd20236d0da56492c325dce289b4da35b4b5bd88ac"),
+            ),
+            TxOut(
+                1049154982,
+                bytes.fromhex("76a914da169b45781ca210f8c11617ba66bd843da76b1688ac"),
+            ),
+        ],
+    )
+    sighash = get_sighash(tx, previous_txout, 0, 0x01)
+    assert dsa._verify(sighash, bytes.fromhex(pubkey), bytes.fromhex(signature)[:-1])
+
+
+# the following tests are taken from python-bitcoinlib tests
+def test_p2pk():
+    pubkey = "0479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8"
+    signature = "304402200A5C6163F07B8D3B013C4D1D6DBA25E780B39658D79BA37AF7057A3B7F15FFA102201FD9B4EAA9943F734928B99A83592C2E7BF342EA2680F6A2BB705167966B742001"
+    scriptPubKey = script.serialize([pubkey, "OP_CHECKSIG"])
+    scriptSig = script.serialize([signature])
+
+    founding_tx_script = script.serialize([0, 0])
+    founding_tx = Tx(
+        1,
+        0,
+        vin=[
+            TxIn(OutPoint(b"\x00" * 32, 0xFFFFFFFF), founding_tx_script, 0xFFFFFFFF, [])
+        ],
+        vout=[TxOut(0, scriptPubKey)],
+    )
+    receiving_tx = Tx(
+        1,
+        0,
+        vin=[TxIn(OutPoint(founding_tx.txid, 0), scriptSig, 0xFFFFFFFF, [])],
+        vout=[TxOut(0, b"")],
+    )
+    sighash = get_sighash(receiving_tx, founding_tx.vout[0], 0, 0x01)
+    assert dsa._verify(sighash, bytes.fromhex(pubkey), bytes.fromhex(signature)[:-1])
+
+
+def test_p2pkh():
+    pubkey = "038282263212C609D9EA2A6E3E172DE238D8C39CABD5AC1CA10646E23FD5F51508"
+    signature = "304402206E05A6FE23C59196FFE176C9DDC31E73A9885638F9D1328D47C0C703863B8876022076FEB53811AA5B04E0E79F938EB19906CC5E67548BC555A8E8B8B0FC603D840C01"
+    scriptPubKey = script.serialize(
+        [
+            "OP_DUP",
+            "OP_HASH160",
+            "1018853670F9F3B0582C5B9EE8CE93764AC32B93",
+            "OP_EQUALVERIFY",
+            "OP_CHECKSIG",
+        ]
+    )
+    scriptSig = script.serialize([signature, pubkey])
+
+    founding_tx_script = script.serialize([0, 0])
+    founding_tx = Tx(
+        1,
+        0,
+        vin=[
+            TxIn(OutPoint(b"\x00" * 32, 0xFFFFFFFF), founding_tx_script, 0xFFFFFFFF, [])
+        ],
+        vout=[TxOut(0, scriptPubKey)],
+    )
+    receiving_tx = Tx(
+        1,
+        0,
+        vin=[TxIn(OutPoint(founding_tx.txid, 0), scriptSig, 0xFFFFFFFF, [])],
+        vout=[TxOut(0, b"")],
+    )
+    sighash = get_sighash(receiving_tx, founding_tx.vout[0], 0, 0x01)
+    assert dsa._verify(sighash, bytes.fromhex(pubkey), bytes.fromhex(signature)[:-1])
+
+
+def test_p2pk_anyonecanpay():
+    pubkey = "048282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f5150811f8a8098557dfe45e8256e830b60ace62d613ac2f7b17bed31b6eaff6e26caf"
+    signature = "304402204710a85181663b32d25c70ec2bbd14adff5ddfff6cb50d09e155ef5f541fc86c0220056b0cc949be9386ecc5f6c2ac0493269031dbb185781db90171b54ac127790281"
+    scriptPubKey = script.serialize([pubkey, "OP_CHECKSIG"])
+    scriptSig = script.serialize([signature])
+
+    founding_tx_script = script.serialize([0, 0])
+    founding_tx = Tx(
+        1,
+        0,
+        vin=[
+            TxIn(OutPoint(b"\x00" * 32, 0xFFFFFFFF), founding_tx_script, 0xFFFFFFFF, [])
+        ],
+        vout=[TxOut(0, scriptPubKey)],
+    )
+    receiving_tx = Tx(
+        1,
+        0,
+        vin=[TxIn(OutPoint(founding_tx.txid, 0), scriptSig, 0xFFFFFFFF, [])],
+        vout=[TxOut(0, b"")],
+    )
+    sighash = get_sighash(receiving_tx, founding_tx.vout[0], 0, 0x81)
+    assert dsa._verify(sighash, bytes.fromhex(pubkey), bytes.fromhex(signature)[:-1])
+
+
+def test_sighashsingle_bug():
+    pubkey = "02D5C25ADB51B61339D2B05315791E21BBE80EA470A49DB0135720983C905AACE0"
+    signature = "3045022100C9CDD08798A28AF9D1BAF44A6C77BCC7E279F47DC487C8C899911BC48FEAFFCC0220503C5C50AE3998A733263C5C0F7061B483E2B56C4C41B456E7D2F5A78A74C07703"
+    scriptPubKey = script.serialize(
+        [
+            "OP_DUP",
+            "OP_HASH160",
+            "5b6462475454710f3c22f5fdf0b40704c92f25c3",
+            "OP_EQUALVERIFY",
+            "OP_CHECKSIGVERIFY",
+            1,
+        ]
+    )
+
+    previous_txout = TxOut(0, scriptPubKey)
+    tx = Tx.deserialize(
+        "01000000020002000000000000000000000000000000000000000000000000000000000000000000000151ffffffff0001000000000000000000000000000000000000000000000000000000000000000000006b483045022100c9cdd08798a28af9d1baf44a6c77bcc7e279f47dc487c8c899911bc48feaffcc0220503c5c50ae3998a733263c5c0f7061b483e2b56c4c41b456e7d2f5a78a74c077032102d5c25adb51b61339d2b05315791e21bbe80ea470a49db0135720983c905aace0ffffffff010000000000000000015100000000"
+    )
+    sighash = get_sighash(tx, previous_txout, 1, 0x03)
+    assert dsa._verify(sighash, bytes.fromhex(pubkey), bytes.fromhex(signature)[:-1])
+
+
+def test_sighash_json():
+    path = os.path.join(os.path.dirname(__file__), "test_data", "legacy_sighash.json")
+    with open(path) as f:
+        data = json.load(f)
+    data = data[1:]
+    for raw_transaction, raw_script, input_index, hashType, sighash in data:
+        if hashType < 0:
+            hashType = 0xFFFFFFFF + 1 + hashType
+        tx = Tx.deserialize(raw_transaction)
+        script = raw_script
+        sighash = bytes.fromhex(sighash)[::-1].hex()
+        scriptCode = _get_legacy_scriptCodes(script)[0]
+        assert sighash == legacy_sighash(scriptCode, tx, input_index, hashType).hex()
--- btclib/tests/test_segwitv0_sighash.py
+++ btclib/tests/test_segwitv0_sighash.py
@@ -11,7 +11,11 @@
 "Tests for `btclib.sighash` module."
 
 # test vector at https://github.com/bitcoin/bips/blob/master/bip-0143.mediawiki
-from btclib import script, tx, tx_out
+from btclib import script
+
+# from btclib.tx_in import TxIn, OutPoint
+from btclib.tx_out import TxOut
+from btclib.tx import Tx
 from btclib.sighash import (
     _get_witness_v0_scriptCodes,
     get_sighash,
@@ -20,11 +24,11 @@ from btclib.sighash import (
 
 
 def test_native_p2wpkh():
-    transaction = tx.Tx.deserialize(
+    transaction = Tx.deserialize(
         "0100000002fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f0000000000eeffffffef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a0100000000ffffffff02202cb206000000001976a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac11000000"
     )
 
-    previous_txout = tx_out.TxOut(
+    previous_txout = TxOut(
         value=600000000,
         scriptPubKey=script.deserialize("00141d0f172a0ecb48aee1be1f2687d2963ae33f71a1"),
     )
@@ -38,14 +42,14 @@ def test_native_p2wpkh():
 
 
 def test_wrapped_p2wpkh():
-    transaction = tx.Tx.deserialize(
+    transaction = Tx.deserialize(
         "0100000001db6b1b20aa0fd7b23880be2ecbd4a98130974cf4748fb66092ac4d3ceb1a54770100000000feffffff02b8b4eb0b000000001976a914a457b684d7f0d539a46a45bbc043f35b59d0d96388ac0008af2f000000001976a914fd270b1ee6abcaea97fea7ad0402e8bd8ad6d77c88ac92040000"
     )
     transaction.vin[0].scriptSig = script.deserialize(
         "001479091972186c449eb1ded22b78e40d009bdf0089"
     )
 
-    previous_txout = tx_out.TxOut(
+    previous_txout = TxOut(
         value=1000000000,
         scriptPubKey=script.deserialize(
             "a9144733f37cf4db86fbc2efed2500b4f4e49f31202387"
@@ -61,14 +65,14 @@ def test_wrapped_p2wpkh():
 
 
 def test_native_p2wsh():
-    transaction = tx.Tx.deserialize(
+    transaction = Tx.deserialize(
         "0100000002fe3dc9208094f3ffd12645477b3dc56f60ec4fa8e6f5d67c565d1c6b9216b36e0000000000ffffffff0815cf020f013ed6cf91d29f4202e8a58726b1ac6c79da47c23d1bee0a6925f80000000000ffffffff0100f2052a010000001976a914a30741f8145e5acadf23f751864167f32e0963f788ac00000000"
     )
     transaction.vin[1].txinwitness = [
         "21026dccc749adc2a9d0d89497ac511f760f45c47dc5ed9cf352a58ac706453880aeadab210255a9626aebf5e29c0e6538428ba0d1dcf6ca98ffdf086aa8ced5e0d0215ea465ac"
     ]
 
-    previous_txout = tx.TxOut(
+    previous_txout = TxOut(
         value=4900000000,
         scriptPubKey=script.deserialize(
             "00205d1b56b63d714eebe542309525f484b7e9d6f686b3781b6f61ef925d66d6f6a0"
@@ -93,7 +97,7 @@ def test_native_p2wsh():
 
 
 def test_native_p2wsh_2():
-    transaction = tx.Tx.deserialize(
+    transaction = Tx.deserialize(
         "0100000002e9b542c5176808107ff1df906f46bb1f2583b16112b95ee5380665ba7fcfc0010000000000ffffffff80e68831516392fcd100d186b3c2c7b95c80b53c77e77c35ba03a66b429a2a1b0000000000ffffffff0280969800000000001976a914de4b231626ef508c9a74a8517e6783c0546d6b2888ac80969800000000001976a9146648a8cd4531e1ec47f35916de8e259237294d1e88ac00000000"
     )
     transaction.vin[0].txinwitness = [
@@ -103,7 +107,7 @@ def test_native_p2wsh_2():
         "5163ab68210392972e2eb617b2388771abe27235fd5ac44af8e61693261550447a4c3e39da98ac"
     ]
 
-    previous_txout_1 = tx_out.TxOut(
+    previous_txout_1 = TxOut(
         value=16777215,
         scriptPubKey=script.deserialize(
             "0020ba468eea561b26301e4cf69fa34bde4ad60c81e70f059f045ca9a79931004a4d"
@@ -115,7 +119,7 @@ def test_native_p2wsh_2():
         == "e9071e75e25b8a1e298a72f0d2e9f4f95a0f5cdf86a533cda597eb402ed13b3a"
     )
 
-    previous_txout_2 = tx.TxOut(
+    previous_txout_2 = TxOut(
         value=16777215,
         scriptPubKey=script.deserialize(
             "0020d9bbfbe56af7c4b7f960a70d7ea107156913d9e5a26b0a71429df5e097ca6537"
@@ -136,14 +140,14 @@ def test_native_p2wsh_2():
 
 def test_wrapped_p2wsh():
 
-    transaction = tx.Tx.deserialize(
+    transaction = Tx.deserialize(
         "010000000136641869ca081e70f394c6948e8af409e18b619df2ed74aa106c1ca29787b96e0100000000ffffffff0200e9a435000000001976a914389ffce9cd9ae88dcc0631e88a821ffdbe9bfe2688acc0832f05000000001976a9147480a33f950689af511e6e84c138dbbd3c3ee41588ac00000000"
     )
     transaction.vin[0].txinwitness = [
         "56210307b8ae49ac90a048e9b53357a2354b3334e9c8bee813ecb98e99a7e07e8c3ba32103b28f0c28bfab54554ae8c658ac5c3e0ce6e79ad336331f78c428dd43eea8449b21034b8113d703413d57761b8b9781957b8c0ac1dfe69f492580ca4195f50376ba4a21033400f6afecb833092a9a21cfdf1ed1376e58c5d1f47de74683123987e967a8f42103a6d48b1131e94ba04d9737d61acdaa1322008af9602b3b14862c07a1789aac162102d8b661b0b3302ee2f162b09e07a55ad5dfbe673a9f01d9f0c19617681024306b56ae"
     ]
 
-    previous_txout = tx_out.TxOut(
+    previous_txout = TxOut(
         value=987654321,
         scriptPubKey=script.deserialize(
             "0020a16b5755f7f6f96dbd65f5f0d6ab9418b89af4b1f14a1bb8a09062c35f0dcb54"
