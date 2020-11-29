c0fc2ac0dbdc3682e1c63011d3cd429947d7ae31
fametrano <ferdinando@ametrano.net>
2020-11-29 12:27:06 +0100

    added safety check

--- btclib/varbytes.py
+++ btclib/varbytes.py
@@ -16,11 +16,14 @@ from .exceptions import BTClibRuntimeError
 from .utils import bytes_from_octets, bytesio_from_binarydata
 
 
-def deserialize(stream: BinaryData) -> bytes:
+def deserialize(stream: BinaryData, forbid_zero_size=False) -> bytes:
     """Return the variable-length octets read from a stream."""
 
     stream = bytesio_from_binarydata(stream)
     i = varint.deserialize(stream)
+    if forbid_zero_size and i == 0:
+        raise BTClibRuntimeError("zero size")
+
     result = stream.read(i)
     if len(result) != i:
         raise BTClibRuntimeError("not enough binary data")
