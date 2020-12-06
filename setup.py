2df38ee496e9213328b332d67b252ebf922426de
fametrano <ferdinando@ametrano.net>
2020-12-06 14:44:33 +0100

    removed redundant specifications

--- setup.py
+++ setup.py
@@ -39,10 +39,6 @@ setup(
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
