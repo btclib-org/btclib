b6c5c2f5b4f1c23b70e8425206dc0fda408712e6
fametrano <ferdinando@ametrano.net>
2020-12-30 00:57:46 +0100

    splitted file

--- btclib/tx/tx_in.py
+++ btclib/tx/tx_in.py
@@ -16,75 +16,9 @@ from dataclasses_json import DataClassJsonMixin, config
 from btclib import var_bytes
 from btclib.alias import BinaryData
 from btclib.exceptions import BTClibValueError
+from btclib.tx.out_point import OutPoint
 from btclib.tx.witness import Witness
 from btclib.utils import bytesio_from_binarydata
-
-_OutPoint = TypeVar("_OutPoint", bound="OutPoint")
-
-
-@dataclass
-class OutPoint(DataClassJsonMixin):
-    tx_id: bytes = field(
-        default=b"\x00" * 32,
-        metadata=config(
-            field_name="txid", encoder=lambda v: v.hex(), decoder=bytes.fromhex
-        ),
-    )
-    vout: int = 0xFFFFFFFF
-    # TODO add value and script_pub_key when tx fetcher will be available
-    check_validity: InitVar[bool] = True
-
-    @property
-    def hash(self) -> int:
-        "Return the hash int for compatibility with COutPoint."
-        return int.from_bytes(self.tx_id, "big", signed=False)
-
-    @property
-    def n(self) -> int:
-        "Return the n int for compatibility with COutPoint."
-        return self.vout
-
-    def __post_init__(self, check_validity: bool) -> None:
-        if check_validity:
-            self.assert_valid()
-
-    def is_coinbase(self) -> bool:
-        return self.tx_id == b"\x00" * 32 and self.vout == 0xFFFFFFFF
-
-    def assert_valid(self) -> None:
-        if len(self.tx_id) != 32:
-            err_msg = f"invalid OutPoint tx_id: {len(self.tx_id)}"
-            err_msg += " instead of 32 bytes"
-            raise BTClibValueError(err_msg)
-        # must be a 4-bytes int
-        if not 0 <= self.vout <= 0xFFFFFFFF:
-            raise BTClibValueError(f"invalid vout: {self.vout}")
-        # not a coinbase, not a regular OutPoint
-        if (self.tx_id == b"\x00" * 32) ^ (self.vout == 0xFFFFFFFF):
-            raise BTClibValueError("invalid OutPoint")
-
-    def serialize(self, check_validity: bool = True) -> bytes:
-        "Return the 36 bytes serialization of the OutPoint."
-
-        if check_validity:
-            self.assert_valid()
-
-        out = self.tx_id[::-1]
-        out += self.vout.to_bytes(4, byteorder="little", signed=False)
-        return out
-
-    @classmethod
-    def parse(
-        cls: Type[_OutPoint], data: BinaryData, check_validity: bool = True
-    ) -> _OutPoint:
-        "Return an OutPoint from the first 36 bytes of the provided data."
-
-        data = bytesio_from_binarydata(data)
-        tx_id = data.read(32)[::-1]
-        vout = int.from_bytes(data.read(4), "little", signed=False)
-
-        return cls(tx_id, vout, check_validity)
-
 
 _TxIn = TypeVar("_TxIn", bound="TxIn")
 
---
+++ tests/tx/test_out_point.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2020-2020 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"Tests for the `btclib.tx_in` module."
+
+import json
+from os import path
+
+import pytest
+
+from btclib.exceptions import BTClibValueError
+from btclib.tx.out_point import OutPoint
+from btclib.tx.tx import Tx
+
+
+def test_out_point() -> None:
+    out_point = OutPoint()
+    assert out_point.tx_id == b"\x00" * 32
+    assert out_point.vout == 0xFFFFFFFF
+    assert out_point.hash == int.from_bytes(out_point.tx_id, "big", signed=False)
+    assert out_point.n == out_point.vout
+    assert out_point.is_coinbase()
+    assert out_point == OutPoint.parse(out_point.serialize())
+
+    tx_id = bytes.fromhex(
+        "d5b5982254eebca64e4b42a3092a10bfb76ab430455b2bf0cf7c4f7f32db1c2e"
+    )
+    vout = 0
+    out_point = OutPoint(tx_id, vout)
+    assert out_point.tx_id == tx_id
+    assert out_point.vout == vout
+    assert out_point.hash == int.from_bytes(out_point.tx_id, "big", signed=False)
+    assert out_point.n == out_point.vout
+    assert not out_point.is_coinbase()
+    assert out_point == OutPoint.parse(out_point.serialize())
+
+
+def test_dataclasses_json_dict_outpoint() -> None:
+    fname = "d4f3c2c3c218be868c77ae31bedb497e2f908d6ee5bbbe91e4933e6da680c970.bin"
+    filename = path.join(path.dirname(__file__), "_data", fname)
+    with open(filename, "rb") as binary_file_:
+        temp = Tx.parse(binary_file_.read())
+
+    prev_out_data = temp.vin[0].prev_out
+
+    # dataclass
+    assert isinstance(prev_out_data, OutPoint)
+
+    # Tx to/from dict
+    tx_in_dict = prev_out_data.to_dict()
+    assert isinstance(tx_in_dict, dict)
+    assert prev_out_data == OutPoint.from_dict(tx_in_dict)
+
+    datadir = path.join(path.dirname(__file__), "_generated_files")
+
+    # Tx dict to/from dict file
+    filename = path.join(datadir, "out_point.json")
+    with open(filename, "w") as file_:
+        json.dump(tx_in_dict, file_, indent=4)
+    with open(filename, "r") as file_:
+        tx_dict2 = json.load(file_)
+    assert isinstance(tx_dict2, dict)
+    assert tx_in_dict == tx_dict2
+
+
+def test_invalid_outpoint() -> None:
+
+    out_point = OutPoint(b"\x01" * 31, 18, check_validity=False)
+    with pytest.raises(BTClibValueError, match="invalid OutPoint tx_id: "):
+        out_point.assert_valid()
+
+    out_point = OutPoint(b"\x01" * 32, -1, check_validity=False)
+    with pytest.raises(BTClibValueError, match="invalid vout: "):
+        out_point.assert_valid()
+
+    out_point = OutPoint(b"\x01" * 32, 0xFFFFFFFF + 1, check_validity=False)
+    with pytest.raises(BTClibValueError, match="invalid vout: "):
+        out_point.assert_valid()
+
+    out_point = OutPoint(b"\x00" * 31 + b"\x01", 0xFFFFFFFF, check_validity=False)
+    with pytest.raises(BTClibValueError, match="invalid OutPoint"):
+        out_point.assert_valid()
+
+    out_point = OutPoint(b"\x00" * 32, 0, check_validity=False)
+    with pytest.raises(BTClibValueError, match="invalid OutPoint"):
+        out_point.assert_valid()
