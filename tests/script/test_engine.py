9ab5005f61fdc62cd9b58e7bae42830e96d159fe
Giacomo Caironi <giacomo.caironi@gmail.com>
2021-08-04 22:40:58 +0200

    First commit of script engine

---
+++ tests/script/test_engine.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017-2021 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"Tests for the `btclib.script.engine` module."
+
+from btclib.script.engine import verify_input
+
+import json
+from os import path
+
+from btclib.script.witness import Witness
+from btclib.tx.tx import Tx
+from btclib.tx.tx_out import TxOut
+
+
+def test_valid_taproot_key_path() -> None:
+    fname = "tapscript_test_vector.json"
+    filename = path.join(path.dirname(__file__), "_data", fname)
+    with open(filename, "r") as file_:
+        data = json.load(file_)
+
+    for x in filter(lambda x: "TAPROOT" in x["flags"], data):
+
+        tx = Tx.parse(x["tx"])
+
+        prevouts = [TxOut.parse(prevout) for prevout in x["prevouts"]]
+        index = x["index"]
+
+        witness = Witness(x["success"]["witness"])
+        tx.vin[index].script_witness = witness
+
+        verify_input(prevouts, tx, index)
