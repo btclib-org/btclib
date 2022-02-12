162c4ab5e298898b635435a59cc0685ed99b934a
fametrano <ferdinando@ametrano.net>
2022-02-12 08:00:00 +0100

    enforced pythonic style (empty sequence is falsey)

--- tests/script/test_witness.py
+++ tests/script/test_witness.py
@@ -18,7 +18,7 @@ from btclib.script.witness import Witness
 
 def test_witness() -> None:
     witness = Witness()
-    assert witness.stack == []
+    assert not witness.stack
     assert len(witness) == 0
     witness.assert_valid()
     assert witness == Witness.parse(witness.serialize())
