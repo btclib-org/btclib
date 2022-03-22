e836350ec7fbeec9d305ea3bea131dce7823c364
Giacomo Caironi <giacomo.caironi@gmail.com>
2022-03-22 23:18:45 +0100

    Exclude tests directory from package

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     description="A library for 'bitcoin cryptography'",
     long_description=longdescription,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     package_data={"btclib": ["_data/*", "ecc/_data/*", "mnemonic/_data/*", "py.typed"]},
     # test_suite="btclib.tests",
