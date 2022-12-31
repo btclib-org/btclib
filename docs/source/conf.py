c82e5f3f0615606a95f8e6cc228dfaa7835f00fe
fametrano <ferdinando@ametrano.net>
2022-12-31 16:49:58 +0100

    getting ready for new release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2017-2022 The btclib developers"
 author = "The btclib developers"
 
 # The full version, including alpha/beta/rc tags
-release = "2022.7.20"
+release = "2022.12.31"
 
 
 # -- General configuration ---------------------------------------------------
