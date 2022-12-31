0df73757fc9ee121901615791d3cd5022ab97e84
fametrano <ferdinando@ametrano.net>
2022-12-31 22:29:32 +0100

    move block(header) out of tx submodule

--- btclib/blocks.py
+++ btclib/blocks.py
@@ -21,7 +21,7 @@ from btclib import var_bytes, var_int
 from btclib.alias import BinaryData
 from btclib.exceptions import BTClibValueError
 from btclib.hashes import hash256, merkle_root
-from btclib.tx.block_header import BlockHeader
+from btclib.block_header import BlockHeader
 from btclib.tx.tx import Tx
 from btclib.utils import bytesio_from_binarydata, decode_num
 
