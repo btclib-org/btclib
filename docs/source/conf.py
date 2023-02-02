3f8946f9c6239a6feb14a903bd63efc639a373fe
fametrano <ferdinando@ametrano.net>
2023-02-02 20:37:31 +0100

    bumped up version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ doc.org/en/master/usage/configuration.html
 project = "btclib"
 project_copyright = "2017-2023 The btclib developers"
 author = "The btclib developers"
-release = "2023.2.3"
+release = "2023.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
